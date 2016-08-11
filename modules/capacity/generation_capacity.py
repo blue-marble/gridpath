29cd0532a6b018a68014444c3f13eab3c0daf7f5
Ana Mileva <anamileva@gmail.com>
2016-08-10 18:37:41 -0700

    Determine operational types in operations module.
    Moved dynamic component function describing operational types from
    capacity module to operations module.

--- modules/capacity/generation_capacity.py
+++ modules/capacity/generation_capacity.py
@@ -1,45 +1,11 @@
 #!/usr/bin/env python
 
 import os
-import csv
 from pyomo.environ import *
-
-
-def determine_dynamic_components(m, inputs_directory):
-    """
-    Populate the lists of dynamic components
-    :param m:
-    :param inputs_directory:
-    :return:
-    """
-    with open(os.path.join(inputs_directory, "generation_capacity.tab"), "rb") as generation_capacity_file:
-        generation_capacity_reader = csv.reader(generation_capacity_file, delimiter="\t")
-        generation_capacity_reader.next()  # skip header
-        for row in generation_capacity_reader:
-            generator = row[0]
-            print generator
-            m.headroom_variables[generator] = list()
-
-    with open(os.path.join(inputs_directory, "reserve_generators.tab"), "rb") as reserve_generators_file:
-        reserve_generators_reader = csv.reader(reserve_generators_file, delimiter="\t")
-        reserve_generators_reader.next()  # skip header
-        for row in reserve_generators_reader:
-            generator = row[0]
-            m.headroom_variables[generator].append("Upward_Reserve")
-
-    with open(os.path.join(inputs_directory, "regulation_generators.tab"), "rb") as regulation_generators_file:
-        regulation_generators_reader = csv.reader(regulation_generators_file, delimiter="\t")
-        regulation_generators_reader.next()  # skip header
-        for row in regulation_generators_reader:
-            generator = row[0]
-            m.headroom_variables[generator].append("Provide_Regulation")
 
 
 def add_model_components(m):
     m.GENERATORS = Set()
-    m.RESERVE_GENERATORS = Set(within=m.GENERATORS)
-    m.REGULATION_GENERATORS = Set(within=m.GENERATORS)
-    m.BASELOAD_GENERATORS = Set(within=m.GENERATORS)
 
     m.capacity = Param(m.GENERATORS)
     m.variable_cost = Param(m.GENERATORS)
@@ -49,18 +15,6 @@ def load_model_data(m, data_portal, inputs_directory):
     data_portal.load(filename=os.path.join(inputs_directory, "generation_capacity.tab"),
                      index=m.GENERATORS,
                      param=(m.capacity, m.variable_cost)
-                     )
-
-    data_portal.load(filename=os.path.join(inputs_directory, "reserve_generators.tab"),
-                     set=m.RESERVE_GENERATORS
-                     )
-
-    data_portal.load(filename=os.path.join(inputs_directory, "regulation_generators.tab"),
-                     set=m.REGULATION_GENERATORS
-                     )
-
-    data_portal.load(filename=os.path.join(inputs_directory, "baseload_generators.tab"),
-                     set=m.BASELOAD_GENERATORS
                      )
 
 
--- modules/operations/generation_operations.py
+++ modules/operations/generation_operations.py
@@ -1,9 +1,46 @@
 #!/usr/bin/env python
+import os
+import csv
 
 from pyomo.environ import *
 
 
+def determine_dynamic_components(m, inputs_directory):
+    """
+    Populate the lists of dynamic components
+    :param m:
+    :param inputs_directory:
+    :return:
+    """
+    with open(os.path.join(inputs_directory, "generation_capacity.tab"), "rb") as generation_capacity_file:
+        generation_capacity_reader = csv.reader(generation_capacity_file, delimiter="\t")
+        generation_capacity_reader.next()  # skip header
+        for row in generation_capacity_reader:
+            generator = row[0]
+            print generator
+            m.headroom_variables[generator] = list()
+
+    with open(os.path.join(inputs_directory, "reserve_generators.tab"), "rb") as reserve_generators_file:
+        reserve_generators_reader = csv.reader(reserve_generators_file, delimiter="\t")
+        reserve_generators_reader.next()  # skip header
+        for row in reserve_generators_reader:
+            generator = row[0]
+            m.headroom_variables[generator].append("Upward_Reserve")
+
+    with open(os.path.join(inputs_directory, "regulation_generators.tab"), "rb") as regulation_generators_file:
+        regulation_generators_reader = csv.reader(regulation_generators_file, delimiter="\t")
+        regulation_generators_reader.next()  # skip header
+        for row in regulation_generators_reader:
+            generator = row[0]
+            m.headroom_variables[generator].append("Provide_Regulation")
+
+
 def add_model_components(m):
+
+    # Operational types
+    m.RESERVE_GENERATORS = Set(within=m.GENERATORS)
+    m.REGULATION_GENERATORS = Set(within=m.GENERATORS)
+    m.BASELOAD_GENERATORS = Set(within=m.GENERATORS)
 
     m.Power = Var(m.GENERATORS, m.TIMEPOINTS, within=NonNegativeReals)
 
@@ -60,6 +97,19 @@ def add_model_components(m):
 
     m.total_cost_components.append("Total_Generation_Cost")
 
+
+def load_model_data(m, data_portal, inputs_directory):
+    data_portal.load(filename=os.path.join(inputs_directory, "reserve_generators.tab"),
+                     set=m.RESERVE_GENERATORS
+                     )
+
+    data_portal.load(filename=os.path.join(inputs_directory, "regulation_generators.tab"),
+                     set=m.REGULATION_GENERATORS
+                     )
+
+    data_portal.load(filename=os.path.join(inputs_directory, "baseload_generators.tab"),
+                     set=m.BASELOAD_GENERATORS
+                     )
 
 def export_results(m):
     for g in getattr(m, "GENERATORS"):
