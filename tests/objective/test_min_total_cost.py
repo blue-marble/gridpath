798f57491bcf41362a88227b11cf24e6e84b8ea9
Ana Mileva <anamileva@gmail.com>
2016-12-06 15:34:06 -0800

    Add tests for objective function (min_total_cost) module

---
+++ tests/objective/test_min_total_cost.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+from collections import OrderedDict
+from importlib import import_module
+import os.path
+import sys
+import unittest
+
+from tests.common_functions import create_abstract_model, \
+    add_components_and_load_data
+
+TEST_DATA_DIRECTORY = \
+    os.path.join(os.path.dirname(__file__), "..", "test_data")
+
+# Import prerequisite modules
+PREREQUISITE_MODULE_NAMES = []
+NAME_OF_MODULE_BEING_TESTED = "objective.min_total_cost"
+IMPORTED_PREREQ_MODULES = list()
+for mdl in PREREQUISITE_MODULE_NAMES:
+    try:
+        imported_module = import_module("." + str(mdl), package="modules")
+        IMPORTED_PREREQ_MODULES.append(imported_module)
+    except ImportError:
+        print("ERROR! Module " + str(mdl) + " not found.")
+        sys.exit(1)
+# Import the module we'll test
+try:
+    MODULE_BEING_TESTED = import_module("." + NAME_OF_MODULE_BEING_TESTED,
+                                        package="modules")
+except ImportError:
+    print("ERROR! Couldn't import module " + NAME_OF_MODULE_BEING_TESTED +
+          " to test.")
+
+
+class TestRegulationUp(unittest.TestCase):
+    """
+
+    """
+    def test_add_model_components(self):
+        """
+        Test that there are no errors when adding model components
+        :return:
+        """
+        create_abstract_model(prereq_modules=IMPORTED_PREREQ_MODULES,
+                              module_to_test=MODULE_BEING_TESTED,
+                              test_data_dir=TEST_DATA_DIRECTORY,
+                              horizon="",
+                              stage=""
+                              )
