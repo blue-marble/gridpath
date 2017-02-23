6d5d7768ef6aeb4df2e6f90cf1e0cd54738e051b
Ana Mileva <anamileva@gmail.com>
2017-02-22 18:01:20 -0800

    Fix bug in aggregating RECs
    Sum over only operational RPS-project -- timepoint combinations in
    each period to avoid indexing errors if some RPS projects are not
    available in all periods.

--- gridpath/project/operations/aggregate_recs.py
+++ gridpath/project/operations/aggregate_recs.py
@@ -2,10 +2,11 @@
 # Copyright 2017 Blue Marble Analytics LLC. All rights reserved.
 
 """
-Describe operational constraints on the generation infrastructure.
+Aggregate delivered RPS-eligible power from the project-timepoint level to
+the RPS zone - period level.
 """
 import os.path
-from pyomo.environ import Param, Set, Expression, value
+from pyomo.environ import Param, Set, Expression
 
 from gridpath.auxiliary.dynamic_components import required_operational_modules
 from gridpath.auxiliary.auxiliary import load_operational_type_modules
@@ -28,6 +29,13 @@ def add_model_components(m, d):
         Set(m.RPS_ZONES, within=m.RPS_PROJECTS,
             initialize=determine_rps_generators_by_rps_zone)
 
+    # Get operational RPS projects - timepoints combinatinos
+    m.RPS_PROJECT_OPERATIONAL_TIMEPOINTS = Set(
+        within=m.PROJECT_OPERATIONAL_TIMEPOINTS,
+        rule=lambda mod: [(p, tmp) for (p, tmp) in
+                          mod.PROJECT_OPERATIONAL_TIMEPOINTS
+                          if p in mod.RPS_PROJECTS]
+    )
     # Import needed operational modules
     imported_operational_modules = \
         load_operational_type_modules(getattr(d, required_operational_modules))
@@ -47,9 +55,8 @@ def add_model_components(m, d):
         return imported_operational_modules[gen_op_type]. \
             scheduled_curtailment_rule(mod, g, tmp)
 
-    # TODO: possibly create this only if needed by another module?
     m.Scheduled_Curtailment_MW = Expression(
-        m.PROJECT_OPERATIONAL_TIMEPOINTS, rule=scheduled_curtailment_rule
+        m.RPS_PROJECT_OPERATIONAL_TIMEPOINTS, rule=scheduled_curtailment_rule
     )
 
     def subhourly_curtailment_rule(mod, g, tmp):
@@ -66,9 +73,8 @@ def add_model_components(m, d):
         return imported_operational_modules[gen_op_type]. \
             subhourly_curtailment_rule(mod, g, tmp)
 
-    # TODO: possibly create this only if needed by another module?
     m.Subhourly_Curtailment_MW = Expression(
-        m.PROJECT_OPERATIONAL_TIMEPOINTS, rule=subhourly_curtailment_rule
+        m.RPS_PROJECT_OPERATIONAL_TIMEPOINTS, rule=subhourly_curtailment_rule
     )
 
     def subhourly_energy_delivered_rule(mod, g, tmp):
@@ -85,9 +91,8 @@ def add_model_components(m, d):
         return imported_operational_modules[gen_op_type]. \
             subhourly_energy_delivered_rule(mod, g, tmp)
 
-    # TODO: possibly create this only if needed by another module?
     m.Subhourly_Energy_Delivered_MW = Expression(
-        m.PROJECT_OPERATIONAL_TIMEPOINTS, rule=subhourly_energy_delivered_rule
+        m.RPS_PROJECT_OPERATIONAL_TIMEPOINTS, rule=subhourly_energy_delivered_rule
     )
 
     def rps_energy_provision_rule(mod, z, p):
@@ -108,12 +113,13 @@ def add_model_components(m, d):
                  - mod.Subhourly_Curtailment_MW[g, tmp])
                 * mod.number_of_hours_in_timepoint[tmp]
                 * mod.horizon_weight[mod.horizon[tmp]]
-                for g in mod.RPS_PROJECTS_BY_RPS_ZONE[z]
-                for tmp in mod.TIMEPOINTS_IN_PERIOD[p]
+                for (g, tmp) in mod.RPS_PROJECT_OPERATIONAL_TIMEPOINTS
+                if g in mod.RPS_PROJECTS_BY_RPS_ZONE[z]
+                and tmp in mod.TIMEPOINTS_IN_PERIOD[p]
                 )
 
     m.Total_Delivered_RPS_Energy_MWh = \
-        Expression(m.RPS_ZONES, m.PERIODS,
+        Expression(m.RPS_ZONE_PERIODS_WITH_RPS,
                    rule=rps_energy_provision_rule)
 
     def total_curtailed_rps_energy_rule(mod, z, p):
@@ -130,8 +136,10 @@ def add_model_components(m, d):
                     mod.Subhourly_Energy_Delivered_MW[g, tmp])
                    * mod.number_of_hours_in_timepoint[tmp]
                    * mod.horizon_weight[mod.horizon[tmp]]
-                   for g in mod.RPS_PROJECTS_BY_RPS_ZONE[z]
-                   for tmp in mod.TIMEPOINTS_IN_PERIOD[p])
+                   for (g, tmp) in mod.RPS_PROJECT_OPERATIONAL_TIMEPOINTS
+                   if g in mod.RPS_PROJECTS_BY_RPS_ZONE[z]
+                   and tmp in mod.TIMEPOINTS_IN_PERIOD[p]
+                   )
     # TODO: is this only needed for export and, if so, should it be created on
     # export?
     m.Total_Curtailed_RPS_Energy_MWh = \
