2ecb79d82b6c6940431e89d210de394cbd182af6
Ana Mileva <anamileva@gmail.com>
2019-06-07 15:18:09 -0700

    Basic multiprocessing for scenarios

--- flask_local_server.py
+++ flask_local_server.py
@@ -1,7 +1,8 @@
 from flask import Flask
-from flask import render_template
 from flask_socketio import SocketIO, send, emit
+import multiprocessing
 import os
+import psutil
 import pyutilib.subprocess.GlobalData
 
 # Turn off signal handlers (in order to be able to spawn solvers from a
@@ -13,6 +14,9 @@ import pyutilib.subprocess.GlobalData
 pyutilib.subprocess.GlobalData.DEFINE_SIGNAL_HANDLERS_DEFAULT = False
 
 app = Flask(__name__)
+
+# Global variables
+SCENARIO_STATUS = dict()
 
 #
 # # @app.route('/hello/')
@@ -41,27 +45,26 @@ app = Flask(__name__)
 #     app.run(host='0.0.0.0', port='8000', debug=True)
 
 
+
 # Needed to pip install eventlet
 socketio = SocketIO(app, async_mode='eventlet')
 
+
 @app.route('/')
 def welcome():
-    print('Welcome')
-    return('WELCOME')
+    print('GridPath UI')
 
-@socketio.on('message')
-def data_received(message):
-    print(message)
-    send(message)
 
 @socketio.on('connect')
 def connection():
     print('Electron connection established')
 
-@socketio.on('_run_scenario')
+
 def _run_scenario(message):
+    p = multiprocessing.current_process()
     scenario_name = str(message['scenario'])
     print("Running " + scenario_name)
+    print("Process name and ID: ", p.name, p.pid)
     os.chdir('/Users/ana/dev/gridpath-ui-dev/')
     import run_scenario
     run_scenario.main(
@@ -71,6 +74,58 @@ def _run_scenario(message):
     return("Scenario completed")
 
 
+@socketio.on('launch_scenario_process')
+def launch_scenario_process(message):
+    scenario_name = str(message['scenario'])
+    # TODO: there needs to be a check that this scenario isn't already running
+    process_status = check_scenario_process_status(message=message)
+    if process_status:
+        print("Scenario already running")
+        emit(
+            'scenario_already_running',
+            'scenario already running'
+        )
+    else:
+        print("Starting process for scenario " + scenario_name)
+        p = multiprocessing.Process(
+            target=_run_scenario,
+            name=scenario_name,
+            args=(message,),
+        )
+        p.start()
+
+        print("Sending PID to client ", p.pid)
+        # # TODO: should we be joining
+        # p.join()
+
+        global SCENARIO_STATUS
+        SCENARIO_STATUS[scenario_name] = dict()
+        SCENARIO_STATUS[scenario_name]['process_id'] = p.pid
+
+
+@socketio.on('check_scenario_process_status')
+def check_scenario_process_status(message):
+    """
+    Check if there is any running process that contains the given name processName.
+    """
+    scenario_name = str(message['scenario'])
+    global SCENARIO_STATUS
+    if scenario_name in SCENARIO_STATUS.keys():
+        if SCENARIO_STATUS[scenario_name]['process_id'] is not None:
+            # TODO: will assume running for now, but will need to actually
+            #  check process status later
+            return True
+        else:
+            return False
+    else:
+        return False
+
+
 if __name__ == '__main__':
     print("Running server manually")
-    socketio.run(app, port='8080', debug=True)
+    socketio.run(
+        app,
+        port='8080',
+        debug=True,
+        use_reloader=False  # Reload manually for code changes to take effect
+    )
