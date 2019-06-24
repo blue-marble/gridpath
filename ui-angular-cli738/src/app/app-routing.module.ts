468c3f874011d287bb84e1a8eb6dbf9e44c9fa2d
Ana Mileva <anamileva@gmail.com>
2019-06-24 14:38:58 -0700

    Basic routing

--- ui-angular-cli738/src/app/app-routing.module.ts
+++ ui-angular-cli738/src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { ScenariosComponent } from './scenarios/scenarios.component';
+import { SettingsComponent } from './settings/settings.component';
+
+const appRoutes: Routes = [
+  { path: 'scenarios', component: ScenariosComponent },
+  { path: 'settings', component: SettingsComponent },
+  { path: '',
+    redirectTo: '/scenarios',
+    pathMatch: 'full'
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
+  ],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
--- ui-angular-cli738/src/app/app.module.ts
+++ ui-angular-cli738/src/app/app.module.ts
@@ -2,11 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 import { HttpClientModule } from '@angular/common/http'
+import { AppRoutingModule } from './app-routing.module';
 
 
 import { AppComponent } from './app.component';
 import { ScenariosComponent } from './scenarios/scenarios.component';
-import { IPCTestComponent } from './ipctest/ipctest.component';
 import { SettingsComponent } from './settings/settings.component';
 
 
@@ -14,13 +14,13 @@ import { SettingsComponent } from './settings/settings.component';
   declarations: [
     AppComponent,
     ScenariosComponent,
-    IPCTestComponent,
     SettingsComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
