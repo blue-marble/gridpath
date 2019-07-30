ff0c20f942e17d7bcd27a8e2802ea6abfa887c68
Ana Mileva <anamileva@gmail.com>
2019-07-29 17:36:08 -0700

    Updates to setup
    Add psutil package + some re-formatting.

--- setup.py
+++ setup.py
@@ -1,11 +1,22 @@
 from setuptools import find_packages, setup
 
+extras_doc = ['Sphinx']
+extras_ui = [
+    'eventlet',  # Async mode for SocketIO
+     'Flask',  # Local API server for UI
+     'Flask-RESTful',  # Flask extension for building REST APIs
+     'Flask-SocketIO',  # Client-server communication
+     'psutil'  # Process management
+]
+extras_all = extras_ui + extras_doc
+
+
 setup(name='GridPath',
       version='0.1',
       description='Software for power-system planning',
       url='https://www.gridpath.io',
       maintainer='Blue Marble Analytics LLC',
-      maintainer_email='gridpath@bluemarble.run',
+      maintainer_email='info@gridpath.io',
       license='TBD',
       platforms=["any"],
       keywords=[
@@ -13,17 +24,15 @@ setup(name='GridPath',
           'planning', 'operations'
       ],
       packages=find_packages(),
-      install_requires=['Pyomo',  # Optimization modeling language
-                        'pandas',
-                        ],
+      install_requires=[
+          'Pyomo',  # Optimization modeling language
+          'pandas', # Data-processing
+          'matplotlib'  # Visualization ibrary
+      ],
       extras_require={
-          'documentation': ["Sphinx"],  # Documentation
-          'viz': ['matplotlib'],  # Visualization library
-          'ui': ['eventlet',  # Async mode for SocketIO
-                 'Flask',  # Local API server for UI
-                 'Flask-RESTful',  # Flask extension for building REST APIs
-                 'Flask-SocketIO'  # Client-server communication
-                 ]
-                      },
+          'doc': extras_doc,
+          'ui': extras_ui,
+          'all': extras_all
+      },
       include_package_data=True
       )
