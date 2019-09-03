8d6d4af9dc6e75b680ee2389f4a886eb96d387ae
Ana Mileva <anamileva@gmail.com>
2019-09-02 18:15:14 -0700

    Add component with hardcoded scenarios to compare

--- ui/src/app/scenario-comparison/scenario-comparison.component.ts
+++ ui/src/app/scenario-comparison/scenario-comparison.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { ScenarioDetailService } from '../scenario-detail/scenario-detail.service';
+import { SettingsTable } from '../scenario-new/scenario-new';
+import { ScenarioNewService } from '../scenario-new/scenario-new.service';
+import { StartingValues } from '../scenario-detail/scenario-detail';
 
 @Component({
   selector: 'app-scenario-comparison',
@@ -7,9 +12,67 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ScenarioComparisonComponent implements OnInit {
 
-  constructor() { }
+  baseScenarioID: number;
+  scenariosIDsToCompare: number[];
+  settingTables: SettingsTable[];
+  baseScenarioValues: StartingValues;
+  scenariosToCompareValues: StartingValues[];
+
+  constructor(
+    private location: Location,
+    private scenarioNewService: ScenarioNewService,
+    private scenarioDetailService: ScenarioDetailService
+  ) { }
 
   ngOnInit() {
+
+    // Get these from form
+    this.baseScenarioID = 1;
+    this.scenariosIDsToCompare = [2, 3, 4];
+
+    this.settingTables = [];
+    this.getSettingTables();
+
+    this.baseScenarioValues = {} as StartingValues;
+    this.getBaseScenarioValues(this.baseScenarioID);
+
+    this.scenariosToCompareValues = [] as StartingValues[];
+    this.getScenariosToCompareValues(this.scenariosIDsToCompare);
   }
 
+  getSettingTables(): void {
+    this.scenarioNewService.getScenarioNewAPI()
+      .subscribe(
+        scenarioNewAPI => {
+          this.settingTables = scenarioNewAPI.SettingsTables;
+        }
+      );
+  }
+
+  getBaseScenarioValues(scenarioID): void {
+    this.scenarioDetailService.getScenarioDetailAPI(scenarioID)
+      .subscribe(
+        scenarioDetail => {
+            this.baseScenarioValues = scenarioDetail.editScenarioValues;
+        }
+      );
+  }
+
+  getScenariosToCompareValues(scenarioIDsToCompare): void {
+    for (const scenarioID of scenarioIDsToCompare) {
+      this.scenarioDetailService.getScenarioDetailAPI(scenarioID)
+        .subscribe(
+          scenarioDetail => {
+              this.scenariosToCompareValues.push(
+                scenarioDetail.editScenarioValues
+              );
+          }
+        );
+    }
+  }
+
+
+  goBack(): void {
+    this.location.back();
+  }
 }
