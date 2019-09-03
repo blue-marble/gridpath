946cf8e85e2d5287d134e23b3f995ee3caf02dc5
Ana Mileva <anamileva@gmail.com>
2019-09-03 10:56:20 -0700

    Clean up

--- ui/src/app/scenario-comparison/scenario-comparison-select.component.ts
+++ ui/src/app/scenario-comparison/scenario-comparison-select.component.ts
@@ -13,18 +13,8 @@ import { Scenario } from '../scenarios/scenarios.component';
 })
 export class ScenarioComparisonSelectComponent implements OnInit {
 
-  // allScenarios: {id: number, name: string}[];
-  //
-  // form: FormGroup;
-  // orders = [
-  //   { id: 100, name: 'order 1' },
-  //   { id: 200, name: 'order 2' },
-  //   { id: 300, name: 'order 3' },
-  //   { id: 400, name: 'order 4' }
-  // ];
-
   scenariosToCompareForm: FormGroup;
-  scenarios: {id: number, name: string}[];
+  allScenarios: {id: number, name: string}[];
 
   constructor(
     private location: Location,
@@ -33,80 +23,42 @@ export class ScenarioComparisonSelectComponent implements OnInit {
     private scenariosService: ScenariosService
   ) {
 
-    // this.baseScenarioForm = this.formBuilder.group({
-    //   scenariosListRadio: new FormArray([])
-    // });
-    //
-    // this.getScenarios();
-    //
-    // this.form = this.formBuilder.group({
-    //   orders: new FormArray([])
-    // });
-
-    // this.baseScenarioForm = this.formBuilder.group({
-    //   scenarios: new FormArray([])
-    // });
     this.scenariosToCompareForm = this.formBuilder.group({
-      scenarios: new FormArray([]),
-      baseScenario: new FormControl()
+      baseScenario: new FormControl(),
+      scenariosToCompare: new FormArray([])
     });
 
-    // this.addCheckboxes();
-    this.scenarios = [];
+    this.allScenarios = [];
     this.getScenarios();
   }
 
-  ngOnInit() {
-
-    // this.allScenarios = [];
-
-  }
+  ngOnInit() { }
 
   getScenarios(): void {
     this.scenariosService.getScenarios()
       .subscribe(scenarios => {
 
         for (const scenario of scenarios) {
-          this.scenarios.push(
+          this.allScenarios.push(
             {id: scenario.id, name: scenario.name}
           );
         }
 
-        this.scenarios.map((o, i) => {
-          const controlCompare = new FormControl();
-          (this.scenariosToCompareForm.controls.scenarios as FormArray).push(controlCompare);
+        // Add form controls for each scenario in the FormArray
+        this.allScenarios.map((o, i) => {
+          (this.scenariosToCompareForm.controls.scenariosToCompare as FormArray).push(
+            new FormControl());
         });
     });
   }
 
-  // private addCheckboxes() {
-  //   this.orders.map((o, i) => {
-  //     const control = new FormControl(i === 0); // if first item set to true, else false
-  //     (this.form.controls.orders as FormArray).push(control);
-  //   });
-  // }
-  //
-  // private addScenarioCheckboxes() {
-  //   this.scenarios.map((o, i) => {
-  //     const control = new FormControl(i === 0); // if first item set to true, else false
-  //     (this.scenariosToCompareForm.controls.scenarios as FormArray).push(control);
-  //   });
-  // }
-  //
-  // submit() {
-  //   const selectedOrderIds = this.form.value.orders
-  //     .map((v, i) => v ? this.orders[i].id : null)
-  //     .filter(v => v !== null);
-  //   console.log(selectedOrderIds);
-  // }
-
   compareScenarioInputs(): void {
-    const selectedScenarioIDs = this.scenariosToCompareForm.value.scenarios
-      .map((v, i) => v ? this.scenarios[i].id : null)
+    const selectedScenarioIDs = this.scenariosToCompareForm.value.scenariosToCompare
+      .map((v, i) => v ? this.allScenarios[i].id : null)
       .filter(v => v !== null);
     const baseScenarioIDToCompare = this.scenariosToCompareForm.value.baseScenario;
-    console.log(selectedScenarioIDs);
-    console.log('Base ', baseScenarioIDToCompare);
+    console.log('Base: ', baseScenarioIDToCompare);
+    console.log('Compare: ', selectedScenarioIDs);
 
     // Switch to the scenario-comparison view with the given base scenario
     // and list of scenarios to compare
@@ -116,7 +68,9 @@ export class ScenarioComparisonSelectComponent implements OnInit {
         scenariosIDsToCompare: selectedScenarioIDs
       }
     };
-    this.router.navigate(['/scenario-comparison'], navigationExtras);
+    this.router.navigate(
+      ['/scenario-comparison'], navigationExtras
+    );
   }
 
   compareScenarioResults(): void {
