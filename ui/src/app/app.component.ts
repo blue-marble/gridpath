d8b016c47ca30f1dcbbf2f10f922f664625145a0
Ana Mileva <anamileva@gmail.com>
2019-09-09 17:23:08 -0700

    Common SocketIO connect function for Angular modules

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
+const io = (window as any).require('socket.io-client');
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,13 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
   title = 'Welcome to GridPath';
+}
+
+// SocketIO connect function to be used by Angular modules
+export function socketConnect() {
+  const socket = io.connect('http://localhost:8080/');
+  socket.on('connect', () => {
+    console.log(`Connection established: ${socket.connected}`);
+  });
+  return socket;
 }
