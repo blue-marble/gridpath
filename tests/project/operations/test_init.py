443e1db1751c925082c3fb46223141b8527922b4
Ana Mileva <anamileva@gmail.com>
2019-09-13 15:14:38 -0700

    Change test

--- tests/project/operations/test_init.py
+++ tests/project/operations/test_init.py
@@ -390,72 +390,31 @@ class TestOperationsInit(unittest.TestCase):
                              actual_tmps_by_startup_fuel_project)
 
         # Param: availability_derate
-        expected_availability = OrderedDict(sorted(
-            {("Nuclear", 202001): 1, ("Nuclear", 202002): 0.5,
-             ("Nuclear", 203001): 0.75, ("Nuclear", 203002): 1,
-             ("Gas_CCGT", 202001): 1, ("Gas_CCGT", 202002): 1,
-             ("Gas_CCGT", 203001): 1, ("Gas_CCGT", 203002): 1,
-             ("Coal", 202001): 0.5, ("Coal", 202002): 1,
-             ("Coal", 203001): 0.75, ("Coal", 203002): 1,
-             ("Gas_CT", 202001): 1, ("Gas_CT", 202002): 1,
-             ("Gas_CT", 203001): 1, ("Gas_CT", 203002): 1,
-             ("Wind", 202001): 1, ("Wind", 202002): 1,
-             ("Wind", 203001): 1, ("Wind", 203002): 1,
-             ("Gas_CCGT_New", 202001): 1, ("Gas_CCGT_New", 202002): 1,
-             ("Gas_CCGT_New", 203001): 1, ("Gas_CCGT_New", 203002): 1,
-             ("Gas_CT_New", 202001): 1, ("Gas_CT_New", 202002): 1,
-             ("Gas_CT_New", 203001): 1, ("Gas_CT_New", 203002): 1,
-             ("Nuclear_z2", 202001): 1, ("Nuclear_z2", 202002): 0.5,
-             ("Nuclear_z2", 203001): 0.75, ("Nuclear_z2", 203002): 1,
-             ("Gas_CCGT_z2", 202001): 1, ("Gas_CCGT_z2", 202002): 1,
-             ("Gas_CCGT_z2", 203001): 1, ("Gas_CCGT_z2", 203002): 1,
-             ("Coal_z2", 202001): 0.5, ("Coal_z2", 202002): 1,
-             ("Coal_z2", 203001): 0.75, ("Coal_z2", 203002): 1,
-             ("Gas_CT_z2", 202001): 1, ("Gas_CT_z2", 202002): 1,
-             ("Gas_CT_z2", 203001): 1, ("Gas_CT_z2", 203002): 1,
-             ("Wind_z2", 202001): 1, ("Wind_z2", 202002): 1,
-             ("Wind_z2", 203001): 1, ("Wind_z2", 203002): 1,
-             ("Battery", 202001): 1, ("Battery", 202002): 1,
-             ("Battery", 203001): 1, ("Battery", 203002): 1,
-             ("Battery_Specified", 202001): 1,
-             ("Battery_Specified", 202002): 1,
-             ("Battery_Specified", 203001): 1,
-             ("Battery_Specified", 203002): 1,
-             ("Battery", 202001): 1, ("Battery", 202002): 1,
-             ("Battery", 203001): 1, ("Battery", 203002): 1,
-             ("Hydro", 202001): 1, ("Hydro", 202002): 1,
-             ("Hydro", 203001): 1, ("Hydro", 203002): 1,
-             ("Hydro_NonCurtailable", 202001): 1,
-             ("Hydro_NonCurtailable", 202002): 1,
-             ("Hydro_NonCurtailable", 203001): 1,
-             ("Hydro_NonCurtailable", 203002): 1,
-             ("Disp_Binary_Commit", 202001): 1,
-             ("Disp_Binary_Commit", 202002): 1,
-             ("Disp_Binary_Commit", 203001): 1,
-             ("Disp_Binary_Commit", 203002): 1,
-             ("Disp_Cont_Commit", 202001): 1, ("Disp_Cont_Commit", 202002): 1,
-             ("Disp_Cont_Commit", 203001): 1, ("Disp_Cont_Commit", 203002): 1,
-             ("Disp_No_Commit", 202001): 1, ("Disp_No_Commit", 202002): 1,
-             ("Disp_No_Commit", 203001): 1, ("Disp_No_Commit", 203002): 1,
-             ("Clunky_Old_Gen", 202001): 1, ("Clunky_Old_Gen", 202002): 1,
-             ("Clunky_Old_Gen", 203001): 1, ("Clunky_Old_Gen", 203002): 1,
-             ("Clunky_Old_Gen2", 202001): 1, ("Clunky_Old_Gen2", 202002): 1,
-             ("Clunky_Old_Gen2", 203001): 1, ("Clunky_Old_Gen2", 203002): 1,
-             ("Customer_PV", 202001): 1, ("Customer_PV", 202002): 1,
-             ("Customer_PV", 203001): 1, ("Customer_PV", 203002): 1,
-             ("Nuclear_Flexible", 202001): 1, ("Nuclear_Flexible", 202002): 1,
-             ("Nuclear_Flexible", 203001): 1, ("Nuclear_Flexible", 203002): 1,
-             ("Shift_DR", 202001): 1, ("Shift_DR", 202002): 1,
-             ("Shift_DR", 203001): 1, ("Shift_DR", 203002): 1,
-             }.items()
+        availability_df = pd.read_csv(
+            os.path.join(TEST_DATA_DIRECTORY, "inputs",
+                         "project_availability.tab"),
+            sep="\t"
         )
-        )
+        defaults = {
+            (p, tmp): 1 for p in instance.PROJECTS
+            for tmp in instance.TIMEPOINTS
+        }
+        derates = {
+            (p, tmp): avail for p, tmp, avail
+            in zip(availability_df.project, availability_df.timepoint,
+                   availability_df.availability_derate)
+        }
+        expected_availability = dict()
+        for (p, tmp) in defaults.keys():
+            if (p, tmp) in derates.keys():
+                expected_availability[p, tmp] = derates[p, tmp]
+            else:
+                expected_availability[p, tmp] = defaults[p, tmp]
 
-        actual_availability = OrderedDict(sorted(
-            {(p, h): instance.availability_derate[p, h] for p in
-             instance.PROJECTS for h in instance.HORIZONS}.items()
-        )
-        )
+        actual_availability = {
+            (p, tmp): instance.availability_derate[p, tmp]
+            for p in instance.PROJECTS for tmp in instance.TIMEPOINTS
+        }
 
         self.assertDictEqual(expected_availability, actual_availability)
 
