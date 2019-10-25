9efc48868c848b086ad36e0cc089393452b4404e
Ana Mileva <anamileva@gmail.com>
2019-10-25 15:11:18 -0700

    Update module tests

--- tests/geography/test_load_zones.py
+++ tests/geography/test_load_zones.py
@@ -3,6 +3,7 @@
 
 from __future__ import print_function
 
+from collections import OrderedDict
 from importlib import import_module
 import os.path
 import unittest
@@ -69,6 +70,56 @@ class TestLoadZones(unittest.TestCase):
         self.assertListEqual(expected, actual,
                              msg="LOAD_ZONES set data does not load correctly."
                              )
+
+        # Param: allow_overgeneration
+        expected_allow_overgen = OrderedDict(
+            sorted({"Zone1": 1, "Zone2": 1}.items())
+        )
+        actual_allow_overgen = OrderedDict(
+            sorted(
+                {z: instance.allow_overgeneration[z]
+                 for z in instance.LOAD_ZONES}.items()
+            )
+        )
+        self.assertDictEqual(expected_allow_overgen, actual_allow_overgen)
+
+        # Param: overgeneration_penalty_per_mw
+        expected_overgen_penalty = OrderedDict(
+            sorted({"Zone1": 99999999, "Zone2": 99999999}.items())
+        )
+        actual_overgen_penalty = OrderedDict(
+            sorted(
+                {z: instance.overgeneration_penalty_per_mw[z]
+                 for z in instance.LOAD_ZONES}.items()
+            )
+        )
+        self.assertDictEqual(expected_overgen_penalty, actual_overgen_penalty)
+
+        # Param: allow_unserved_energy
+        expected_allow_unserved_energy = OrderedDict(
+            sorted({"Zone1": 1, "Zone2": 1}.items())
+        )
+        actual_allow_unserved_energy = OrderedDict(
+            sorted(
+                {z: instance.allow_overgeneration[z]
+                 for z in instance.LOAD_ZONES}.items()
+            )
+        )
+        self.assertDictEqual(expected_allow_unserved_energy,
+                             actual_allow_unserved_energy)
+
+        # Param: unserved_energy_penalty_per_mw
+        expected_unserved_energy_penalty = OrderedDict(
+            sorted({"Zone1": 99999999, "Zone2": 99999999}.items())
+        )
+        actual_unserved_energy_penalty = OrderedDict(
+            sorted(
+                {z: instance.unserved_energy_penalty_per_mw[z]
+                 for z in instance.LOAD_ZONES}.items()
+            )
+        )
+        self.assertDictEqual(expected_unserved_energy_penalty,
+                             actual_unserved_energy_penalty)
 
 
 if __name__ == "__main__":
