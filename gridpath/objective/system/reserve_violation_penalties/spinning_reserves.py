9b45e7d12cdc7c3639d56b8f6c3f5923555e0f61
Ana Mileva <anamileva@gmail.com>
2019-10-28 10:44:22 -0700

    Clean-up

--- gridpath/objective/system/reserve_violation_penalties/spinning_reserves.py
+++ gridpath/objective/system/reserve_violation_penalties/spinning_reserves.py
@@ -5,7 +5,7 @@
 from __future__ import absolute_import
 
 from .aggregate_reserve_violation_penalties import \
-    generic_add_model_components, generic_load_model_data
+    generic_add_model_components
 
 
 def add_model_components(m, d):
@@ -24,11 +24,3 @@ def add_model_components(m, d):
         "spinning_reserves_violation_penalty_per_mw",
         "Spinning_Reserves_Penalty_Costs"
         )
-
-
-def load_model_data(m, d, data_portal, scenario_directory, subproblem, stage):
-    generic_load_model_data(m, d, data_portal,
-                            scenario_directory, subproblem, stage,
-                            "spinning_reserves_balancing_areas.tab",
-                            "spinning_reserves_violation_penalty_per_mw"
-                            )
