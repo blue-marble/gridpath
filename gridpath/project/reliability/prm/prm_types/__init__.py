3aa495ea42b3e8c8193f071667b26728a7589ab3
Ana Mileva <anamileva@gmail.com>
2019-12-18 14:56:05 -0800

    More updates to get_inputs_from_database queries

--- gridpath/project/reliability/prm/prm_types/__init__.py
+++ gridpath/project/reliability/prm/prm_types/__init__.py
@@ -138,15 +138,13 @@ def get_required_prm_type_modules(subscenarios, c):
             FROM 
             (SELECT project
             FROM inputs_project_prm_zones
-            WHERE prm_zone_scenario_id = {}
-            AND project_prm_zone_scenario_id = {}) as proj_tbl
+            WHERE project_prm_zone_scenario_id = {}) as proj_tbl
             LEFT OUTER JOIN 
             (SELECT project, prm_type
             FROM inputs_project_elcc_chars
             WHERE project_elcc_chars_scenario_id = {}
             AND prm_type IS NOT NULL) as prm_type_tbl
             USING (project);""".format(
-                subscenarios.PRM_ZONE_SCENARIO_ID,
                 subscenarios.PROJECT_PRM_ZONE_SCENARIO_ID,
                 subscenarios.PROJECT_ELCC_CHARS_SCENARIO_ID
             )
