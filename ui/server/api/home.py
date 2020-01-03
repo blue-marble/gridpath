4921e09c42fffad9356bb696484702501457b4c0
Ana Mileva <anamileva@users.noreply.github.com>
2020-01-03 15:51:33 -0800

    Add scenario status to UI home screen (#292)
    Show number of scenarios by status type.

--- ui/server/api/home.py
+++ ui/server/api/home.py
@@ -2,6 +2,8 @@
 
 from flask_restful import Resource
 
+from db.common_functions import connect_to_database
+
 
 class ServerStatus(Resource):
     """
@@ -12,3 +14,48 @@ class ServerStatus(Resource):
     @staticmethod
     def get():
         return 'running'
+
+
+class ScenarioRunStatus(Resource):
+    """
+    Number of scenarios by run status type.
+    """
+    def __init__(self, **kwargs):
+        self.db_path = kwargs["db_path"]
+
+    def get(self):
+        conn = connect_to_database(db_path=self.db_path)
+        c = conn.cursor()
+
+        run_status_api = c.execute("""
+            SELECT run_status_name, COUNT(run_status_id)
+            FROM scenarios
+            JOIN mod_run_status_types
+              USING (run_status_id)
+            GROUP BY run_status_name
+        """).fetchall()
+
+        return run_status_api
+
+
+class ScenarioValidationStatus(Resource):
+    """
+    Number of scenarios by validation status type.
+    """
+
+    def __init__(self, **kwargs):
+        self.db_path = kwargs["db_path"]
+
+    def get(self):
+        conn = connect_to_database(db_path=self.db_path)
+        c = conn.cursor()
+
+        validation_status_api = c.execute("""
+                SELECT validation_status_name, COUNT(validation_status_id)
+                FROM scenarios
+                JOIN mod_validation_status_types
+                  USING (validation_status_id)
+                GROUP BY validation_status_name
+            """).fetchall()
+
+        return validation_status_api
