5b4e8616121ede12f8df799543acd69995406b3e
Ana Mileva <anamileva@gmail.com>
2020-01-14 16:20:32 -0800

    Save plot data function iterates over list of scenarios

--- ui/server/save_data.py
+++ ui/server/save_data.py
@@ -6,6 +6,7 @@ Functions to save data from the UI to CSVs.
 
 import csv
 from importlib import import_module
+import pandas as pd
 
 from db.common_functions import connect_to_database
 from ui.server.api.view_data import get_table_data as get_results_table_data
@@ -23,6 +24,9 @@ def save_table_data_to_csv(db_path, download_path, scenario_id,
     :param scenario_id:
     :param other_scenarios:
     :param table:
+    :param table_type:
+    :param ui_table_name_in_db:
+    :param ui_row_name_in_db:
     :return:
     """
     print(table)
@@ -53,13 +57,13 @@ def save_table_data_to_csv(db_path, download_path, scenario_id,
             writer.writerow(values)
 
 
-def save_plot_data_to_csv(db_path, download_path, scenario_id, plot_type,
+def save_plot_data_to_csv(db_path, download_path, scenario_id_list, plot_type,
                           load_zone, carbon_cap_zone, rps_zone,
                           period, horizon, subproblem, stage, project):
     """
     :param db_path: string, the path to the database
     :param download_path: string, the CSV file path
-    :param scenario_id: integer, the scenario_id
+    :param scenario_id_list: list of integers, the scenario_ids to get data for
     :param plot_type: string, which plot
     :param load_zone: string, load zone parameter for the plot
     :param carbon_cap_zone: string, carbon cap zone parameter for the plot
@@ -80,26 +84,40 @@ def save_plot_data_to_csv(db_path, download_path, scenario_id, plot_type,
     # Connect to the database
     conn = connect_to_database(db_path=db_path)
 
-    # Import viz module, get the dataframe, and export it to CSV
+    # Import viz module, get the dataframes for all scenarios, and add them to
+    # a list
+    df_list = []
     try:
         imp_m = \
             import_module(
               "." + plot_type,
               package="viz"
             )
-        df = imp_m.get_plotting_data(
-            conn=conn,
-            scenario_id=scenario_id,
-            load_zone=load_zone,
-            carbon_cap_zone=carbon_cap_zone,
-            rps_zone=rps_zone,
-            period=period,
-            horizon=horizon,
-            subproblem=subproblem,
-            stage=stage,
-            project=project,
-        )
-        df.to_csv(download_path, index=False)
+        for scenario_id in scenario_id_list:
+            df = imp_m.get_plotting_data(
+                conn=conn,
+                scenario_id=scenario_id,
+                load_zone=load_zone,
+                carbon_cap_zone=carbon_cap_zone,
+                rps_zone=rps_zone,
+                period=period,
+                horizon=horizon,
+                subproblem=subproblem,
+                stage=stage,
+                project=project,
+            )
+            df.insert(0, "scenario_name",
+                      conn.cursor().execute("""
+                      SELECT scenario_name
+                      FROM scenarios
+                      WHERE scenario_id = {};
+                      """.format(scenario_id)
+                                          ).fetchone()[0]
+                      )
+            df_list.append(df)
     except ImportError:
         print("ERROR! Visualization module " + plot_type + " not found.")
+
+    export_df = pd.concat(df_list)
+    export_df.to_csv(download_path, index=False)
 
