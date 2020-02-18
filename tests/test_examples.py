3220c1086e647769367c9165865fdfabfa5e016e
Gerrit De Moor <gerrit.dm@gmail.com>
2020-02-18 12:39:04 -0500

    Add db functionality for binary build types
     - add functions to db utilities and csv_to_db utilities
     - add entries to csv_data_master
     - fix some errors in gen_new_bin module
     - add test_new_binary_solar to test examples

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -647,6 +647,24 @@ class TestExamples(unittest.TestCase):
         self.assertAlmostEqual(expected_objective, actual_objective,
                                places=1)
 
+    def test_example_test_new_binary_storage(self):
+        """
+        Check objective function value of "test_new_binary_storage" example
+        :return:
+        """
+        actual_objective = \
+            run_scenario.main(
+                ["--scenario",
+                 "test_new_binary_storage",
+                 "--scenario_location", EXAMPLES_DIRECTORY,
+                 "--quiet", "--mute_solver_output", "--testing"]
+            )
+
+        expected_objective = 51243.96
+
+        self.assertAlmostEqual(expected_objective, actual_objective,
+                               places=1)
+
     def test_example_test_new_solar_carbon_cap(self):
         """
         Check objective function value of "test_new_solar_carbon_cap" example
