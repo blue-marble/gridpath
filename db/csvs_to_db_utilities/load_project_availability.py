bbc268cda7341ae372e9e9bfdcf46bdf82363e15
Gerrit <gerrit.dm@gmail.com>
2020-02-26 15:25:24 -0700

    Fix db operations for endogenous availability (#418)
     - add db utils
     - add csv to db utils
     - fix db schema error
     - fix SQL errors in binary and continuous availability

--- db/csvs_to_db_utilities/load_project_availability.py
+++ db/csvs_to_db_utilities/load_project_availability.py
@@ -9,6 +9,24 @@ import numpy as np
 from collections import OrderedDict
 from db.utilities import project_availability
 
+
+# TODO: move to common_functions, also used in load_transmission_new_costs
+def recur_dictify(frame):
+    """
+    Converts DataFrame to nested dictionary using recursion.
+
+    :param frame:
+    :return:
+    """
+    if len(frame.columns) == 1:
+        if frame.values.size == 1:
+            return frame.values[0][0]
+        return frame.values.squeeze()
+    grouped = frame.groupby(frame.columns[0])
+    d = {k: recur_dictify(g.iloc[:, 1:]) for k, g in grouped}
+    return d
+
+
 def load_project_availability_types(io, c, subscenario_input, data_input):
     """
     Project availability types dictionary has project as the key with availability type and the two
@@ -57,6 +75,7 @@ def load_project_availability_types(io, c, subscenario_input, data_input):
             project_types_and_char_ids=project_availability_types
         )
 
+
 def load_project_availability_exogenous(io, c, subscenario_input, data_input):
     """
     :param io:
@@ -99,3 +118,61 @@ def load_project_availability_exogenous(io, c, subscenario_input, data_input):
             project_avail=project_availability_exogenous
         )
 
+
+def load_project_availability_endogenous(io, c, subscenario_input, data_input):
+    """
+    :param io:
+    :param c:
+    :param subscenario_input:
+    :param data_input:
+    :return:
+    """
+
+    project_availability_endogenous = dict()
+    project_availability_endogenous_scenarios = dict()
+
+    for i in subscenario_input.index:
+        sc_id = int(subscenario_input['endogenous_availability_scenario_id'][i])
+        sc_name = subscenario_input['name'][i]
+        sc_description = subscenario_input['description'][i]
+        prj = subscenario_input['project'][i]
+
+        project_availability_endogenous_scenarios[prj] = dict()
+        project_availability_endogenous_scenarios[prj][sc_id] = (sc_name, sc_description)
+
+        data_input_subscenario = data_input.loc[
+            (data_input['endogenous_availability_scenario_id'] == sc_id)
+            & (data_input['project'] == prj)
+        ]
+
+        key_cols = ["project", "endogenous_availability_scenario_id"]
+        value_cols = ["unavailable_hours_per_period",
+                      "unavailable_hours_per_event_min",
+                      "unavailable_hours_per_event_max",
+                      "available_hours_between_events_min",
+                      "available_hours_between_events_max"]
+
+        df = data_input_subscenario.groupby(key_cols)[value_cols].apply(
+            lambda x: x.values.tolist()[0]).to_frame().reset_index()
+        project_avail = recur_dictify(df)
+
+        # project_availability_endogenous[sc_id] = OrderedDict()
+        # project_availability_endogenous[sc_id][prj] = OrderedDict()
+        #
+        # project_availability_by_project =
+        #
+        # project_availability_endogenous[sc_id][prj]
+        #
+        #     project_stage_availability_by_project = project_availability_by_project.loc[
+        #         project_availability_by_project['stage_id'] == st_id, ['timepoint', 'availability_derate']]
+        #     project_stage_availability_by_project[['timepoint']] = \
+        #         project_stage_availability_by_project[['timepoint']].astype(int)
+        #     project_availability_endogenous[prj][sc_id][int(st_id)] = \
+        #         project_stage_availability_by_project.set_index('timepoint')[
+        #         'availability_derate'].to_dict()
+
+        project_availability.insert_project_availability_endogenous(
+            io=io, c=c,
+            project_avail_scenarios=project_availability_endogenous_scenarios,
+            project_avail=project_avail
+        )
--- db/port_csvs_to_gridpath.py
+++ db/port_csvs_to_gridpath.py
@@ -279,6 +279,13 @@ def load_csv_data(db_path, csv_path):
         (csv_subscenario_input, csv_data_input) = csvs_read.csv_read_data(data_folder_path)
         load_project_availability.load_project_availability_exogenous(io, c2, csv_subscenario_input, csv_data_input)
 
+    ## PROJECT AVAILABILITY ENDOGENOUS ##
+    if csv_data_master.loc[csv_data_master['table'] == 'project_availability_endogenous', 'include'].iloc[0] == 1:
+        data_folder_path = os.path.join(folder_path, csv_data_master.loc[
+            csv_data_master['table'] == 'project_availability_endogenous', 'path'].iloc[0])
+        (csv_subscenario_input, csv_data_input) = csvs_read.csv_read_data(data_folder_path)
+        load_project_availability.load_project_availability_endogenous(io,  c2, csv_subscenario_input, csv_data_input)
+
     #### LOAD PROJECT HEAT RATE DATA ####
 
     ## PROJCT HEAT RATES ##
