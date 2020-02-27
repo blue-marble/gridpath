03dead370ec1fdaa74dff6389a6f80a682c4e79a
Gerrit De Moor <gerrit.dm@gmail.com>
2020-02-26 20:52:02 -0700

    Fix duplicate ELCC chars from rebasing.

--- db/port_csvs_to_gridpath.py
+++ db/port_csvs_to_gridpath.py
@@ -275,17 +275,6 @@ def load_csv_data(conn, csv_path):
         load_project_local_capacity_chars.load_project_local_capacity_chars(
             conn, c2, csv_subscenario_input, csv_data_input)
 
-    ## PROJECT ELCC CHARS ##
-    if csv_data_master.loc[csv_data_master['table'] ==
-                           'project_elcc_chars', 'include'].iloc[
-        0] == 1:
-        data_folder_path = os.path.join(folder_path, csv_data_master.loc[
-            csv_data_master['table'] == 'project_elcc_chars',
-            'path'].iloc[0])
-        (csv_subscenario_input, csv_data_input) = csvs_read.csv_read_data(data_folder_path)
-        load_project_prm.load_project_prm(
-            conn, c2, csv_subscenario_input, csv_data_input)
-
     #### LOAD PROJECT AVAILABILITY DATA ####
 
     ## PROJECT AVAILABILITY TYPES ##
