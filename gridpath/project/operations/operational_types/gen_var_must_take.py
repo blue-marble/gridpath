2e732c3fbfa5551d970a8ab0169d1a88abf01bf1
Ana Mileva <anamileva@users.noreply.github.com>
2020-03-03 09:06:06 -0800

    Refactor first/last-timepoint-of-horizon check (#436)

--- gridpath/project/operations/operational_types/gen_var_must_take.py
+++ gridpath/project/operations/operational_types/gen_var_must_take.py
@@ -20,6 +20,8 @@ from gridpath.auxiliary.auxiliary import generator_subset_init, \
     check_projects_for_reserves
 from gridpath.auxiliary.dynamic_components import headroom_variables, \
     footroom_variables
+from gridpath.project.common_functions import \
+    check_if_linear_horizon_first_timepoint
 
 
 def add_module_specific_components(m, d):
@@ -242,10 +244,9 @@ def power_delta_rule(mod, g, tmp):
     """
     Exogenously defined ramp for variable must-take generators.
     """
-    if tmp == mod.first_horizon_timepoint[
-        mod.horizon[tmp, mod.balancing_type_project[g]]] \
-            and mod.boundary[mod.horizon[tmp, mod.balancing_type_project[g]]] \
-            == "linear":
+    if check_if_linear_horizon_first_timepoint(
+        mod=mod, tmp=tmp, balancing_type=mod.balancing_type_project[g]
+    ):
         pass
     else:
         return \
