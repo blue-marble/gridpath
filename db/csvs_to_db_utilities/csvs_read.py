823b7314a9416c971a155755ddb18cf013811f89
Ana Mileva <anamileva@gmail.com>
2020-03-14 18:39:27 -0700

    Fix bug of not importing all subscenarios

--- db/csvs_to_db_utilities/csvs_read.py
+++ db/csvs_to_db_utilities/csvs_read.py
@@ -21,31 +21,32 @@ def csv_read_data(folder_path, quiet):
     # subscenario file. The file only needs the string 'subscenario' in its name. The function will then read
     # each subscenario data using the filename in the subscenario file. All other files will be ignored.
 
+    csv_subscenario = pd.DataFrame(columns=["id", "name", "description"])
     csv_data = pd.DataFrame()
     data_starting_from_row = 1  # Notes rows start from 0. So this is the 6th row in the csv.
 
-    # ### WILL NEED TO REMOVE FROM HERE ###
-    # for f in os.listdir(folder_path):
-    #     if f.endswith(".csv") and 'template' not in f and 'subscenario' in f and 'ignore' not in f:
-    #         if not quiet:
-    #             print(f)
-    #         csv_subscenario = pd.read_csv(os.path.join(folder_path, f))
-    #         print(csv_subscenario)
-    #
-    # for row in range(0, len(csv_subscenario.index)):
-    #     subscenario_filename = csv_subscenario.iloc[row]['filename']
-    #     if '.csv' not in subscenario_filename:
-    #         subscenario_filename = subscenario_filename + '.csv'
-    #     if not quiet:
-    #         print(subscenario_filename)
-    #     csv_data = csv_data.append(
-    #         pd.read_csv(os.path.join(folder_path, subscenario_filename)))
-    # ### TO HERE ###
+    ### WILL NEED TO REMOVE FROM HERE ###
+    for f in os.listdir(folder_path):
+        if f.endswith(".csv") and 'template' not in f and 'subscenario' in f and 'ignore' not in f:
+            if not quiet:
+                print(f)
+            csv_subscenario = pd.read_csv(os.path.join(folder_path, f))
+            print(csv_subscenario)
+
+    for row in range(0, len(csv_subscenario.index)):
+        subscenario_filename = csv_subscenario.iloc[row]['filename']
+        if '.csv' not in subscenario_filename:
+            subscenario_filename = subscenario_filename + '.csv'
+        if not quiet:
+            print(subscenario_filename)
+        csv_data = csv_data.append(
+            pd.read_csv(os.path.join(folder_path, subscenario_filename)))
+    ### TO HERE ###
 
     # Look for CSV files
     # TODO: this does not allow for underscores in the scenario name,
     # so pass count https://stackoverflow.com/questions/30636248/split-a-string-only-by-first-space-in-python
-    csv_subscenario = pd.DataFrame(columns=["id", "name", "description"])
+
     row_number = 0
     for f in os.listdir(folder_path):
         if f.endswith(".csv"):
@@ -59,6 +60,7 @@ def csv_read_data(folder_path, quiet):
             subscenario_data_df = pd.read_csv(os.path.join(folder_path, f))
             subscenario_data_df["id"] = subscenario_id
             csv_data = csv_data.append(subscenario_data_df)
+            row_number += 1
 
     # print(csv_subscenario, csv_data)
 
