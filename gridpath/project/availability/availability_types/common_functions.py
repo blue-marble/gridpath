b22e6527b1a8a3a5a6678cb9b42c24e90e1e81a7
Ana Mileva <anamileva@gmail.com>
2020-03-20 16:43:21 -0700

    Order endogenous availability results in DB

--- gridpath/project/availability/availability_types/common_functions.py
+++ gridpath/project/availability/availability_types/common_functions.py
@@ -19,7 +19,41 @@ def insert_availability_results(
         for row in reader:
             results.append((scenario_id,) + tuple(row))
 
-    # Update the results table with the module-specific results
+    # Get the CREATE statemnt for the persistent table
+    tbl_sql = c.execute("""
+        SELECT sql 
+        FROM sqlite_master
+        WHERE type='table'
+        AND name='results_project_availability_endogenous'
+        """).fetchone()[0]
+
+    # Create a temporary table with the same structure as the persistent table
+    temp_tbl_sql = \
+        tbl_sql.replace(
+            "CREATE TABLE results_project_availability_endogenous",
+            """CREATE TEMPORARY TABLE 
+            temp_results_project_availability_endogenous{}""".format(
+                scenario_id
+            )
+        )
+
+    spin_on_database_lock(conn=db, cursor=c, sql=temp_tbl_sql,
+                          data=(), many=False)
+
+    # Insert the results into the temporary table
+    insert_temp_sql = """
+        INSERT INTO temp_results_project_availability_endogenous{}
+        (scenario_id, project, period, subproblem_id, stage_id, 
+        availability_type, timepoint, timepoint_weight, 
+        number_of_hours_in_timepoint,
+        load_zone, technology, unavailability_decision, start_unavailablity, 
+        stop_unavailability, availability_derate)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
+    """.format(scenario_id)
+
+    spin_on_database_lock(conn=db, cursor=c, sql=insert_temp_sql, data=results)
+
+    # Insert the ordered results into the permanent table
     insert_sql = """
         INSERT INTO results_project_availability_endogenous
         (scenario_id, project, period, subproblem_id, stage_id, 
@@ -27,7 +61,22 @@ def insert_availability_results(
         number_of_hours_in_timepoint,
         load_zone, technology, unavailability_decision, start_unavailablity, 
         stop_unavailability, availability_derate)
-        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
-        """.format(scenario_id)
+        SELECT scenario_id, project, period, subproblem_id, stage_id, 
+        availability_type, timepoint, timepoint_weight, 
+        number_of_hours_in_timepoint,
+        load_zone, technology, unavailability_decision, start_unavailablity, 
+        stop_unavailability, availability_derate
+        FROM temp_results_project_availability_endogenous{}
+        ORDER BY scenario_id, subproblem_id, stage_id, project, timepoint
+    """.format(scenario_id)
 
-    spin_on_database_lock(conn=db, cursor=c, sql=insert_sql, data=results)
+    spin_on_database_lock(conn=db, cursor=c, sql=insert_sql,
+                          data=(), many=False)
+
+    # Drop the temporary table
+    drop_temp_sql = """
+        DROP TABLE temp_results_project_availability_endogenous{}
+    """.format(scenario_id)
+
+    spin_on_database_lock(conn=db, cursor=c, sql=drop_temp_sql,
+                          data=(), many=False)
