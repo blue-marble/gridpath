243a53dd09a959e997a8d068528851287d2b7ba7
Ana Mileva <anamileva@gmail.com>
2020-04-04 15:09:56 -0700

    Add sim flow limits CSV-to-DB functionality

--- db/utilities/simultaneous_flows.py
+++ db/utilities/simultaneous_flows.py
@@ -9,44 +9,34 @@ from db.common_functions import spin_on_database_lock
 from db.common_functions import spin_on_database_lock
 
 
-def insert_transmission_simultaneous_flow_limits(
+def insert_into_database(
         io, c,
-        transmission_simultaneous_flow_limit_scenario_id,
-        scenario_name,
-        scenario_description,
-        group_period_limits
+        subscenario_data,
+        input_data
 ):
     """
 
     :param io: 
     :param c: 
-    :param transmission_simultaneous_flow_limit_scenario_id: 
-    :param scenario_name: 
-    :param scenario_description: 
-    :param group_period_limits: 
-    Two-level dictionary with the names of the transmission line groups as 
-    top-level keys, the period as second key, and the flow limit
-    :return: 
+    :param subscenario_data: list of tuples with subscenario data
+        (transmission_simultaneous_flow_limit_scenario_id, name,
+        description)
+    :param input_data: list of tuples with data for all subscenarios
+         (transmission_simultaneous_flow_limit_scenario_id,
+         transmission_simultaneous_flow_limit, period, max_flow_mw)
     """
     # Subscenarios
-    subs_data = [(transmission_simultaneous_flow_limit_scenario_id,
-            scenario_name, scenario_description)]
     subs_sql = """
         INSERT OR IGNORE INTO subscenarios_transmission_simultaneous_flow_limits
         (transmission_simultaneous_flow_limit_scenario_id, name,
         description)
         VALUES (?, ?, ?);
         """
-    spin_on_database_lock(conn=io, cursor=c, sql=subs_sql, data=subs_data)
+    spin_on_database_lock(
+        conn=io, cursor=c, sql=subs_sql, data=subscenario_data
+    )
 
     # Insert data
-    inputs_data = []
-    for group in list(group_period_limits.keys()):
-        for period in list(group_period_limits[group].keys()):
-            inputs_data.append(
-                (transmission_simultaneous_flow_limit_scenario_id,
-                    group, period, group_period_limits[group][period])
-            )
     inputs_sql = """
         INSERT OR IGNORE INTO
         inputs_transmission_simultaneous_flow_limits
@@ -55,4 +45,4 @@ def insert_transmission_simultaneous_flow_limits(
         max_flow_mw)
         VALUES (?, ?, ?, ?);
         """
-    spin_on_database_lock(conn=io, cursor=c, sql=inputs_sql, data=inputs_data)
+    spin_on_database_lock(conn=io, cursor=c, sql=inputs_sql, data=input_data)
