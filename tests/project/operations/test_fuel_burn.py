ee4b79b4b8255a12accf45af8e3e3010e8018b1d
Ana Mileva <anamileva@gmail.com>
2020-08-25 14:08:07 -0700

    Add tests for costs and fuel_burn modules

--- tests/project/operations/test_fuel_burn.py
+++ tests/project/operations/test_fuel_burn.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python
 # Copyright 2017 Blue Marble Analytics LLC. All rights reserved.
 
-from __future__ import print_function
-
-from builtins import str
-from collections import OrderedDict
 from importlib import import_module
 import os.path
 import pandas as pd
@@ -13,6 +9,8 @@ import unittest
 
 from tests.common_functions import create_abstract_model, \
     add_components_and_load_data
+from tests.project.operations.common_functions import \
+    get_project_operational_timepoints
 
 TEST_DATA_DIRECTORY = \
     os.path.join(os.path.dirname(__file__), "..", "..", "test_data")
@@ -85,27 +83,120 @@ class TestFuelBurn(unittest.TestCase):
         )
         instance = m.create_instance(data)
 
-        # param fuel
-        # Load test data
+        # Load test data as dataframes
         projects_df = pd.read_csv(
-            os.path.join(TEST_DATA_DIRECTORY, "inputs", "projects.tab"),
-            sep="\t",
-            usecols=['project', 'fuel']
+            os.path.join(TEST_DATA_DIRECTORY, "inputs", "projects.tab")
+            , sep="\t"
         )
 
-        # Check derived param: fuel
-        expected_fuel_df = projects_df[~projects_df['fuel'].isin(['.'])]
-        expected_fuel = OrderedDict(
-            sorted(
-                expected_fuel_df.set_index('project').to_dict()['fuel'].items()
-            )
+        hr_curve_df = pd.read_csv(
+            os.path.join(TEST_DATA_DIRECTORY, "inputs",
+                         "heat_rate_curves.tab")
+            , sep="\t"
         )
-        actual_fuel = OrderedDict(
-            sorted(
-                {prj: instance.fuel[prj] for prj in instance.FUEL_PRJS}.items()
+
+        timepoints_df = \
+            pd.read_csv(
+                os.path.join(TEST_DATA_DIRECTORY, "inputs", "timepoints.tab"),
+                sep="\t", usecols=['timepoint', 'period']
             )
+
+        # Set: FUEL_PRJ_OPR_TMPS
+        expected_fuel_projects = sorted(
+            projects_df[projects_df["fuel"] != "."]["project"].tolist()
         )
-        self.assertDictEqual(expected_fuel, actual_fuel)
+        expected_fuel_prj_tmps = get_project_operational_timepoints(
+            expected_fuel_projects
+        )
+        actual_fuel_prj_tmps = \
+            sorted([(p, tmp) for (p, tmp) in instance.FUEL_PRJ_OPR_TMPS])
+
+        self.assertListEqual(expected_fuel_prj_tmps,
+                             actual_fuel_prj_tmps)
+
+        # Set: HR_CURVE_PRJS_OPR_TMPS
+        expected_hr_curve_projects = sorted(
+            hr_curve_df["project"].unique().tolist()
+        )
+
+        expected_hr_curve_prj_tmps = get_project_operational_timepoints(
+            expected_hr_curve_projects
+        )
+
+        actual_hr_curve_prj_tmps = \
+            sorted([(p, tmp) for (p, tmp) in
+                    instance.HR_CURVE_PRJS_OPR_TMPS])
+
+        self.assertListEqual(expected_hr_curve_prj_tmps,
+                             actual_hr_curve_prj_tmps)
+
+        # Set: HR_CURVE_PRJS_OPR_TMPS_SGMS
+        expected_segments_by_prj_period = {
+            ("Coal", 2020): [0],
+            ("Coal", 2030): [0],
+            ("Gas_CCGT", 2020): [0],
+            ("Gas_CCGT", 2030): [0],
+            ("Gas_CT", 2020): [0],
+            ("Gas_CT", 2030): [0],
+            ("Nuclear", 2020): [0],
+            ("Nuclear", 2030): [0],
+            ("Gas_CCGT_New", 2020): [0],
+            ("Gas_CCGT_New", 2030): [0],
+            ("Gas_CCGT_New_Binary", 2020): [0],
+            ("Gas_CCGT_New_Binary", 2030): [0],
+            ("Gas_CT_New", 2020): [0],
+            ("Gas_CT_New", 2030): [0],
+            ("Coal_z2", 2020): [0],
+            ("Coal_z2", 2030): [0],
+            ("Gas_CCGT_z2", 2020): [0],
+            ("Gas_CCGT_z2", 2030): [0],
+            ("Gas_CT_z2", 2020): [0],
+            ("Gas_CT_z2", 2030): [0],
+            ("Nuclear_z2", 2020): [0],
+            ("Nuclear_z2", 2030): [0],
+            ("Disp_Binary_Commit", 2020): [0],
+            ("Disp_Binary_Commit", 2030): [0],
+            ("Disp_Cont_Commit", 2020): [0],
+            ("Disp_Cont_Commit", 2030): [0],
+            ("Disp_No_Commit", 2020): [0],
+            ("Disp_No_Commit", 2030): [0],
+            ("Clunky_Old_Gen", 2020): [0],
+            ("Clunky_Old_Gen", 2030): [0],
+            ("Clunky_Old_Gen2", 2020): [0],
+            ("Clunky_Old_Gen2", 2030): [0],
+            ("Nuclear_Flexible", 2020): [0],
+            ("Nuclear_Flexible", 2030): [0]
+        }
+        expected_hr_curve_prj_tmp_sgms = list()
+        for (prj, tmp) in expected_hr_curve_prj_tmps:
+            prd = timepoints_df[
+                timepoints_df["timepoint"] == tmp
+                ].iloc[0]["period"]
+            segments = expected_segments_by_prj_period[prj, prd]
+            for sgm in segments:
+                expected_hr_curve_prj_tmp_sgms.append((prj, tmp, sgm))
+
+        actual_hr_curve_prj_tmp_sgms = \
+            sorted([(prj, tmp, sgm) for (prj, tmp, sgm) in
+                    instance.HR_CURVE_PRJS_OPR_TMPS_SGMS])
+
+        self.assertListEqual(expected_hr_curve_prj_tmp_sgms,
+                             actual_hr_curve_prj_tmp_sgms)
+
+        # Set: STARTUP_FUEL_PRJ_OPR_TMPS
+        expected_startup_fuel_projects = sorted(
+            projects_df[projects_df["startup_fuel_mmbtu_per_mw"] != "."][
+                "project"].tolist()
+        )
+        expected_startup_fuel_prj_tmps = get_project_operational_timepoints(
+            expected_startup_fuel_projects
+        )
+        actual_startup_fuel_prj_tmps = \
+            sorted([(p, tmp) for (p, tmp)
+                    in instance.STARTUP_FUEL_PRJ_OPR_TMPS])
+
+        self.assertListEqual(expected_startup_fuel_prj_tmps,
+                             actual_startup_fuel_prj_tmps)
 
 
 if __name__ == "__main__":
