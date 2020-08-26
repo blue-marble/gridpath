4f7089f16f1577661da92ab14108d6d04395ed39
Gerrit De Moor <gerrit.dm@gmail.com>
2020-08-26 14:35:34 -0400

    Refactor using stacked bar and get data from db

--- viz/dashboard/main.py
+++ viz/dashboard/main.py
@@ -6,8 +6,9 @@ To run: navigate ./viz/ folder and run:
 "bokeh serve dashboard --show"
 
 TODO:
- - NEW: add scenario ID as drop down
- - NEW: link capacity plots and eneryg plot to real data
+ - NEW: add scenario ID as drop down (and allow multiple)
+ - NEW: allow multiple zones
+ - NEW: link capacity plots and energy plot to real data
  - NEW: link summary plot to real data
  - replace dummy data with real sql scripts pulling data
      also requires adding database connection and parsers
@@ -30,6 +31,7 @@ TODO:
    across scenarios, specify periods)?
    --> on UI side, would select scenario and have button to generate dashboard,
    or similarly, select period, and generate dashboard that compares scenarios
+ - New req/dependency: sqlalchemy (not sure why we didn't need it before?)
 
 Notes:
     General idea should be to load in all data (all periods / load zones)
@@ -47,122 +49,119 @@ Questions:
 """
 
 from argparse import ArgumentParser
-from bokeh.embed import json_item
 from bokeh.models import Tabs, Panel, PreText, Select, ColumnDataSource, \
-    Legend, NumeralTickFormatter, DataTable, TableColumn
-from bokeh.models.tools import HoverTool
-from bokeh.io import curdoc, show
+    DataTable, TableColumn
+from bokeh.io import curdoc
 from bokeh.layouts import column, row
-from bokeh.plotting import figure
-from bokeh.palettes import cividis
 
 import pandas as pd
-import sys
 
 # GridPath modules
 from db.common_functions import connect_to_database
 from gridpath.auxiliary.auxiliary import get_scenario_id_and_name
-from viz.common_functions import show_hide_legend
+from viz.common_functions import create_stacked_bar_plot
 
 # TODO: base on actual data
 ZONE_OPTIONS = ['Zone1', 'Zone2']  # todo: Add "all" option
-CAP_OPTIONS = ['new_cap', 'cumulative_new_cap', 'retired_cap',
-               'cumulative_retired_cap', 'total_cap']
-COST_COMPONENTS = ["capacity_cost", "fuel_cost", "vom_cost", "startup_cost",
-                   "shutdown_cost"]
-
-
-# Data gathering functions
-def get_all_summary_data(conn, scenario_id):
-    df = pd.DataFrame(
-        columns=['period', 'stage', 'load_zone', 'total_cost', 'load',
-                 'average_cost', 'emissions', 'rps_gen_pct', 'curtailment_pct',
-                 'cumulative_new_capacity', 'unserved_energy'],
-        data=[[2020, 1, 'Zone1', 20000, 8000, 50, 3000, 0.6, 0.04, 30000, 0],
-              [2030, 1, 'Zone1', 20000, 8000, 50, 3000, 0.6, 0.04, 30000, 0],
-              [2040, 1, 'Zone1', 20000, 8000, 50, 3000, 0.6, 0.04, 30000, 0],
-              [2020, 1, 'Zone2', 10000, 5000, 40, 3000, 0.5, 0.02, 20000, 0],
-              [2030, 1, 'Zone2', 10000, 5000, 40, 3000, 0.5, 0.02, 20000, 0],
-              [2040, 1, 'Zone2', 20000, 8000, 50, 3000, 0.6, 0.04, 30000, 0],
-              ]
-    )
-    #  TODO: look at results tool RESOLVE for more metrics
-    # 'Unpivot' from wide to long format
-    df = pd.melt(
-        df,
-        id_vars=['period', 'stage', 'load_zone'],
-        var_name='summary_metric',
-        value_name='value'
-    )
-    # TODO: sort metrics in a way that we want them (not alphabetically)
-    df['period'] = df['period'].astype(str)  # Bokeh CDS needs string columns
-
-    # Pivot periods into columms
-    df = pd.pivot_table(
-        df,
-        index=['stage', 'load_zone', 'summary_metric'],
-        columns='period',
-        values='value'
-    ).fillna(0).reset_index()
-
-    # df = df.drop(["stage", "load_zone"], axis=1)
-    return df
+CAP_OPTIONS = ["new_build_capacity", "retired_capacity", "total_capacity",
+               "cumulative_new_build_capacity", "cumulative_retired_capacity"]
+COST_COMPONENTS = ["capacity_cost", "fuel_cost", "variable_om_cost",
+                   "startup_cost", "shutdown_cost"]
+# todo: cost components not used
+SCENARIO = "test"
+scenario_id = 23
+DB_PATH = "../db/test_examples2.db"  # TODO: link to UI or parsed arg?
 
 
 def get_all_cost_data(conn, scenario_id):
     # get data for all zones and periods
-    # TODO: use COST COMPONENTS var?
-    idx_cols = ['period', 'stage', 'load_zone']
-    value_cols = ['capacity_cost', 'fuel_cost', 'vom_cost',
-                  'startup_cost', 'shutdown_cost']
-    df = pd.DataFrame(
-        columns=['period', 'stage', 'load_zone',
-                 'capacity_cost', 'fuel_cost', 'vom_cost',
-                 'startup_cost', 'shutdown_cost'],
-        data=[[2020, 1, 'Zone1', 10, 10, 10, 10, 10],
-              [2020, 1, 'Zone2', 0, 0, 0, 0, 0],
-              [2030, 1, 'Zone1', 20, 20, 20, 20, 20],
-              [2030, 1, 'Zone2', 0, 0, 0, 0, 0],
-              [2040, 1, 'Zone1', 30, 30, 30, 30, 30],
-              [2040, 1, 'Zone2', 20, 20, 20, 20, 20]
-              ]
-    )
+
+    # TODO: add tx deliverability costs, but those aren't by zone!
+    #  might just keep zone NULL and make sure filter can deal with it
+    # TODO: don't filter for certain scenario
+    # TODO: filter for subproblem/stage (?)
+    # idx_cols = ["scenario_id", "subproblem_id", "stage_id", "period",
+    #             "load_zone"]
+    idx_cols = ["period", "load_zone"]
+    value_cols = ["capacity_cost", "variable_om_cost", "fuel_cost",
+                  "startup_cost", "shutdown_cost", "tx_capacity_cost",
+                  "tx_hurdle_cost"]
+    sql = """SELECT {}
+        FROM results_costs_by_period_load_zone
+        WHERE scenario_id = ?;
+        """.format(",".join(idx_cols + value_cols))
+
+    df = pd.read_sql(sql, conn, params=(scenario_id,)).fillna(0)
+
+    # idx_cols = ['period', 'load_zone']
+    # value_cols = ['capacity_cost', 'fuel_cost', 'variable_om_cost',
+    #               'startup_cost', 'shutdown_cost']
+    # df = pd.DataFrame(
+    #     columns=['period', 'load_zone',
+    #              'capacity_cost', 'fuel_cost', 'variable_om_cost',
+    #              'startup_cost', 'shutdown_cost'],
+    #     data=[[2020, 'Zone1', 10, 10, 10, 10, 10],
+    #           [2020, 'Zone2', 0, 0, 0, 0, 0],
+    #           [2030, 'Zone1', 20, 20, 20, 20, 20],
+    #           [2030, 'Zone2', 0, 0, 0, 0, 0],
+    #           [2040, 'Zone1', 30, 30, 30, 30, 30],
+    #           [2040, 'Zone2', 20, 20, 20, 20, 20]
+    #           ]
+    # )
 
     df['period'] = df['period'].astype(str)  # for categorical axis in Bokeh
-
     return df, idx_cols, value_cols
 
 
 def get_all_capacity_data(conn, scenario_id):
-    # get data for all zones and periods
-    df = pd.DataFrame(
-        columns=['period', 'stage', 'load_zone', 'technology'] + CAP_OPTIONS,
-        data=[[2020, 1, 'Zone1', 'Solar', 10, 10, 0, 0, 50],
-              [2030, 1, 'Zone1', 'Solar', 10, 20, 0, 0, 60],
-              [2040, 1, 'Zone1', 'Solar', 10, 30, 0, 0, 70],
-              [2020, 1, 'Zone1', 'Wind', 10, 10, 0, 0, 20],
-              [2030, 1, 'Zone1', 'Wind', 10, 20, 0, 0, 30],
-              [2040, 1, 'Zone1', 'Wind', 10, 30, 0, 0, 40],
-              [2020, 1, 'Zone2', 'Solar', 0, 0, 0, 0, 20],
-              [2030, 1, 'Zone2', 'Solar', 10, 10, 0, 0, 30],
-              [2040, 1, 'Zone2', 'Solar', 10, 20, 0, 0, 40],
-              [2020, 1, 'Zone2', 'Wind', 20, 20, 0, 0, 30],
-              [2030, 1, 'Zone2', 'Wind', 10, 30, 0, 0, 40],
-              [2040, 1, 'Zone2', 'Wind', 10, 40, 0, 0, 50]
-              ]
-    )
+    # TODO: filter for subproblem/stage (?)
+    # TODO: don't return value and idx cols (?)
+    idx_cols = ["period", "load_zone", "technology"]
+    sql = """SELECT {}, 
+        SUM(new_build_mw) AS new_build_capacity, 
+        SUM(retired_mw) AS retired_capacity, 
+        SUM(capacity_mw) AS total_capacity
+        FROM results_project_capacity
+        WHERE scenario_id = ?
+        GROUP BY {};
+        """.format(",".join(idx_cols), ",".join(idx_cols))
+    df = pd.read_sql(sql, conn, params=(scenario_id,)).fillna(0)
 
-    # 'Unpivot' from wide to long format
+    df["cumulative_new_build_capacity"] = df.groupby(
+        ["load_zone", "technology"]
+    )["new_build_capacity"].transform(pd.Series.cumsum)
+    df["cumulative_retired_capacity"] = df.groupby(
+        ["load_zone", "technology"]
+    )["retired_capacity"].transform(pd.Series.cumsum)
+
+    # df = pd.DataFrame(
+    #     columns=['period', 'load_zone', 'technology'] + CAP_OPTIONS,
+    #     data=[[2020, 'Zone1', 'Solar', 10, 10, 0, 0, 50],
+    #           [2030, 'Zone1', 'Solar', 10, 20, 0, 0, 60],
+    #           [2040, 'Zone1', 'Solar', 10, 30, 0, 0, 70],
+    #           [2020, 'Zone1', 'Wind', 10, 10, 0, 0, 20],
+    #           [2030, 'Zone1', 'Wind', 10, 20, 0, 0, 30],
+    #           [2040, 'Zone1', 'Wind', 10, 30, 0, 0, 40],
+    #           [2020, 'Zone2', 'Solar', 0, 0, 0, 0, 20],
+    #           [2030, 'Zone2', 'Solar', 10, 10, 0, 0, 30],
+    #           [2040, 'Zone2', 'Solar', 10, 20, 0, 0, 40],
+    #           [2020, 'Zone2', 'Wind', 20, 20, 0, 0, 30],
+    #           [2030, 'Zone2', 'Wind', 10, 30, 0, 0, 40],
+    #           [2040, 'Zone2', 'Wind', 10, 40, 0, 0, 50]
+    #           ]
+    # )
+
+    # 'Unpivot' capacity metrics from wide to long format
     df = pd.melt(
         df,
-        id_vars=['period', 'stage', 'load_zone', 'technology'],
+        id_vars=['period', 'load_zone', 'technology'],
         var_name='capacity_metric',
         value_name='capacity'
     )
 
     # Pivot technologies to wide format (for stack chart)
     # Note: df.pivot does not work with multi-index as of pandas 1.0.5
-    idx_cols = ['period', 'stage', 'load_zone', 'capacity_metric']
+    idx_cols = ['period', 'load_zone', 'capacity_metric']
     value_cols = list(df['technology'].unique())
     df = pd.pivot_table(
         df,
@@ -172,32 +171,68 @@ def get_all_capacity_data(conn, scenario_id):
     ).fillna(0).reset_index()
 
     df['period'] = df['period'].astype(str)  # for categorical axis in Bokeh
-
     return df, idx_cols, value_cols
+
+
+# Data gathering functions
+def get_all_summary_data(conn, scenario_id):
+    df = pd.DataFrame(
+        columns=['period', 'load_zone', 'total_cost', 'load',
+                 'average_cost', 'emissions', 'rps_gen_pct', 'curtailment_pct',
+                 'cumulative_new_capacity', 'unserved_energy'],
+        data=[[2020, 'Zone1', 20000, 8000, 50, 3000, 0.6, 0.04, 30000, 0],
+              [2030, 'Zone1', 20000, 8000, 50, 3000, 0.6, 0.04, 30000, 0],
+              [2040, 'Zone1', 20000, 8000, 50, 3000, 0.6, 0.04, 30000, 0],
+              [2020, 'Zone2', 10000, 5000, 40, 3000, 0.5, 0.02, 20000, 0],
+              [2030, 'Zone2', 10000, 5000, 40, 3000, 0.5, 0.02, 20000, 0],
+              [2040, 'Zone2', 20000, 8000, 50, 3000, 0.6, 0.04, 30000, 0],
+              ]
+    )
+    #  TODO: look at results tool RESOLVE for more metrics
+    # 'Unpivot' from wide to long format
+    df = pd.melt(
+        df,
+        id_vars=['period', 'load_zone'],
+        var_name='summary_metric',
+        value_name='value'
+    )
+    # TODO: sort metrics in a way that we want them (not alphabetically)
+    df['period'] = df['period'].astype(str)  # Bokeh CDS needs string columns
+
+    # Pivot periods into columms
+    df = pd.pivot_table(
+        df,
+        index=['load_zone', 'summary_metric'],
+        columns='period',
+        values='value'
+    ).fillna(0).reset_index()
+
+    # df = df.drop(["stage", "load_zone"], axis=1)
+    return df
 
 
 def get_all_energy_data(conn, scenario_id):
     # get data for all zones and periods
     df = pd.DataFrame(
-        columns=['period', 'stage', 'load_zone', 'technology', 'energy'],
-        data=[[2020, 1, 'Zone1', 'Solar', 10],
-              [2030, 1, 'Zone1', 'Solar', 10],
-              [2040, 1, 'Zone1', 'Solar', 10],
-              [2020, 1, 'Zone1', 'Wind', 20],
-              [2030, 1, 'Zone1', 'Wind', 20],
-              [2040, 1, 'Zone1', 'Wind', 20],
-              [2020, 1, 'Zone2', 'Solar', 0],
-              [2030, 1, 'Zone2', 'Solar', 10],
-              [2040, 1, 'Zone2', 'Solar', 20],
-              [2020, 1, 'Zone2', 'Wind', 20],
-              [2030, 1, 'Zone2', 'Wind', 30],
-              [2040, 1, 'Zone2', 'Wind', 40]
+        columns=['period', 'load_zone', 'technology', 'energy'],
+        data=[[2020, 'Zone1', 'Solar', 10],
+              [2030, 'Zone1', 'Solar', 10],
+              [2040, 'Zone1', 'Solar', 10],
+              [2020, 'Zone1', 'Wind', 20],
+              [2030, 'Zone1', 'Wind', 20],
+              [2040, 'Zone1', 'Wind', 20],
+              [2020, 'Zone2', 'Solar', 0],
+              [2030, 'Zone2', 'Solar', 10],
+              [2040, 'Zone2', 'Solar', 20],
+              [2020, 'Zone2', 'Wind', 20],
+              [2030, 'Zone2', 'Wind', 30],
+              [2040, 'Zone2', 'Wind', 40]
               ]
     )
 
     # Pivot technologies to wide format (for stack chart)
     # Note: df.pivot does not work with multi-index as of pandas 1.0.5
-    idx_cols = ['period', 'stage', 'load_zone']
+    idx_cols = ['period', 'load_zone']
     value_cols = list(df['technology'].unique())
     df = pd.pivot_table(
         df,
@@ -209,67 +244,6 @@ def get_all_energy_data(conn, scenario_id):
     df['period'] = df['period'].astype(str)  # for categorical axis in Bokeh
 
     return df, idx_cols, value_cols
-
-
-# Plot creation functions
-def get_stacked_plot_by_period(source, stackers, title,
-                               y_axis_label="", legend_title=""):
-    # TODO: configure layout better (less tall)
-    # TODO: add axes
-    # TODO: add legend title
-    # Note: x_range is necessary for categorical plot; make sure dtype is str
-    plot = figure(
-        title=title,
-        x_range=source.data['period'],
-        plot_width=600, plot_height=300,
-
-    )
-    area_renderers = plot.vbar_stack(
-        stackers=stackers,
-        x='period',
-        source=source,
-        color=cividis(len(stackers)),
-        width=0.5
-    )
-    # Note: cannot use legend=stackers because there is an issue when the legend
-    # and column names are the same, see here:
-    # https://github.com/bokeh/bokeh/issues/5365
-
-    # Add Legend
-    legend_items = [(y, [area_renderers[i]]) for i, y in enumerate(stackers)
-                    if source.data[y].mean() > 0]
-    legend = Legend(items=legend_items)
-    plot.add_layout(legend, 'right')
-    plot.legend.title = legend_title
-    plot.legend[0].items.reverse()  # Reverse legend to match stacked order
-    plot.legend.click_policy = 'hide'  # Add interactivity to the legend
-    show_hide_legend(plot=plot)  # Hide legend on double click
-
-    # Format Axes (labels, number formatting, range, etc.)
-    x_axis_label = "Period"
-    plot.xaxis.axis_label = x_axis_label
-    plot.yaxis.axis_label = y_axis_label
-    plot.yaxis.formatter = NumeralTickFormatter(format="0,0")
-    # plot.y_range.end = ylimit  # will be ignored if ylimit is None
-
-    # Add HoverTools
-    for r in area_renderers:
-        group = r.name
-        if "$" in y_axis_label or "USD" in y_axis_label:
-            y_axis_formatter = "@%s{$0,0}" % group
-        else:
-            y_axis_formatter = "@%s{0,0}" % group
-        hover = HoverTool(
-            tooltips=[
-                ("%s" % x_axis_label, "@{period}"),
-                ("%s" % legend_title, group),
-                ("%s" % y_axis_label, y_axis_formatter)
-            ],
-            renderers=[r],
-            toggleable=False)
-        plot.add_tools(hover)
-
-    return plot
 
 
 # Define callbacks
@@ -300,7 +274,7 @@ def update_summary_data(zone="Zone1"):
     :return:
     """
     slice = summary[summary["load_zone"] == zone]
-    slice = slice.drop(['load_zone', 'stage'], axis=1)
+    slice = slice.drop(['load_zone'], axis=1)
     new_src = ColumnDataSource(slice)
     summary_source.data.update(new_src.data)
     # Can als do <cost_source.data = slice> but this is better?
@@ -316,7 +290,9 @@ def update_cost_data(zone="Zone1"):
     :return:
     """
     slice = cost[cost["load_zone"] == zone]
+    slice = slice.drop("load_zone", axis=1)  # drop because not stacked
     new_src = ColumnDataSource(slice)
+    new_src.remove("index")  # only keep stackers and x_col
     cost_source.data.update(new_src.data)
     # Can als do <cost_source.data = slice> but '.update' is better?
 
@@ -329,11 +305,13 @@ def update_energy_data(zone="Zone1"):
     :return:
     """
     slice = energy[energy["load_zone"] == zone]
+    slice = slice.drop("load_zone", axis=1)  # drop because not stacked
     new_src = ColumnDataSource(slice)
+    new_src.remove("index")  # only keep stackers and x_col
     energy_source.data.update(new_src.data)
 
 
-def update_capacity_data(zone="Zone1", capacity_metric="new_cap"):
+def update_capacity_data(zone="Zone1", capacity_metric=CAP_OPTIONS[2]):
     """
     Update the ColumnDataSource object 'cost_source' with the appropriate
     load_zone slice of the data, and with the appropriate capacity metric.
@@ -343,20 +321,22 @@ def update_capacity_data(zone="Zone1", capacity_metric="new_cap"):
     """
     slice = capacity[(capacity["load_zone"] == zone)
                      & (capacity["capacity_metric"] == capacity_metric)]
+    slice = slice.drop(["load_zone", "capacity_metric"], axis=1)  # drop because not stacked
     new_src = ColumnDataSource(slice)
+    new_src.remove("index")  # only keep stackers and x_col
     capacity_source.data.update(new_src.data)
 
 
 # TODO: need to dynamically link initial zone value (or set to "all"?)
 # Set up widgets
 zone_select = Select(title="Load Zone:", value='Zone1', options=ZONE_OPTIONS)
-capacity_select = Select(title="Capacity Metric:", value='new_cap',
+capacity_select = Select(title="Capacity Metric:", value=CAP_OPTIONS[2],
                          options=CAP_OPTIONS)
 
 # Get the data (make sure we do this in global scope vs. in callbacks)
-# TODO: get actual connection and data
-conn = "dbtest"
-scenario_id = 1
+conn = connect_to_database(db_path=DB_PATH)
+
+# TODO: remove value_cols and idx_cols (or start using them)
 summary = get_all_summary_data(conn, scenario_id)
 cost, cost_idx_cols, cost_value_cols = get_all_cost_data(conn, scenario_id)
 capacity, capacity_idx_cols, capacity_value_cols = \
@@ -383,26 +363,27 @@ columns = [TableColumn(field=c, title=c) for c in cols_to_use]
 summary_table = DataTable(columns=columns, source=summary_source, height=250)
 
 # Set up plots
-cost_plot = get_stacked_plot_by_period(
-    cost_source,
-    stackers=cost_value_cols,
+cost_plot = create_stacked_bar_plot(
+    source=cost_source,
+    x_col="period",  # dynamically update this or add scenario?
     title="Cost by Component",
-    legend_title="Cost Component",
-    y_axis_label="Cost (million USD)"  # TODO: link to units
+    category_label="Cost Component",
+    y_label="Cost (million USD)"  # TODO: link to units
+    # TODO:set width and height to resp. (600, 300)
 )
-energy_plot = get_stacked_plot_by_period(
-    energy_source,
-    stackers=energy_value_cols,
+energy_plot = create_stacked_bar_plot(
+    source=energy_source,
+    x_col="period",  # dynamically update this or add scenario?
     title="Energy by Technology",
-    legend_title="Technology",
-    y_axis_label="Energy (MWh)"  # TODO: link to units
+    category_label="Technology",
+    y_label="Energy (MWh)"  # TODO: link to units
 )
-cap_plot = get_stacked_plot_by_period(
-    capacity_source,
-    stackers=capacity_value_cols,
+cap_plot = create_stacked_bar_plot(
+    source=capacity_source,
+    x_col="period",  # dynamically update this or add scenario?
     title="Capacity by Technology",
-    legend_title="Technology",
-    y_axis_label="Capacity (MW)"  # TODO: link to units
+    category_label="Technology",
+    y_label="Capacity (MW)"  # TODO: link to units
 )
 
 active_zone = 'Zone1'
