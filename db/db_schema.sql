blue-marble/gridpath

commit 2a62258ec204b588da9e5e9d948b53d17daae47f

Remove project column from aggregated results (#704)
Missed this in #693.

db/db_schema.sql
@@ -2825,7 +2825,6 @@ PRIMARY KEY (scenario_id, project, subproblem_id, stage_id, timepoint)
 DROP TABLE IF EXISTS results_project_carbon_emissions_by_technology_period;
 CREATE TABLE results_project_carbon_emissions_by_technology_period (
 scenario_id INTEGER,
-project VARCHAR(64),
 period INTEGER,
 subproblem_id INTEGER,
 stage_id INTEGER,

commit 1fecc4f5949d3c858650c5899f30dd68b05bf156

Fix primary key bug introduced in #704

db/db_schema.sql
@@ -2845,7 +2845,8 @@ stage_id INTEGER,
 load_zone VARCHAR(32),
 technology VARCHAR(32),
 carbon_emission_tons FLOAT,
-PRIMARY KEY (scenario_id, project, subproblem_id, stage_id, period)
+PRIMARY KEY (scenario_id, subproblem_id, stage_id, period, load_zone,
+technology)
 );
 
 DROP TABLE IF EXISTS results_project_rps;