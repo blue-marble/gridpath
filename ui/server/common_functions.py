bddfb4c53fe168c7c521c0c87bd68914b602d8aa
Ana Mileva <anamileva@users.noreply.github.com>
2020-10-05 12:32:08 -0700

    Clean up get_executable_path function (#740)

--- ui/server/common_functions.py
+++ ui/server/common_functions.py
@@ -9,17 +9,14 @@ def get_executable_path(script_name):
     :param script_name: string; the name of the GridPath script
     :return: string; the path to the GridPath executable
 
-    Get the entry point script path from the sys.executable (remove 'python'
-    and add the script name)
+    Get the entry point script path from the sys.executable. On Windows,
+    the GridPath scripts are located in a 'Scripts' directory within the base
+    directory of the Python executable; on Mac, the Python executable and
+    the GridPath script are in the same directory.
     """
-    chars_to_remove = 11 if os.name == "nt" else 7
-
-    base_dir = os.path.basename(sys.executable[:-chars_to_remove])
-
-    gridpath_executable = \
-        os.path.join(
-          sys.executable[:-chars_to_remove],
-          "" if base_dir.lower() in ["scripts", "bin"] else "scripts",
+    gridpath_executable = os.path.join(
+          os.path.dirname(sys.executable),
+          "Scripts" if os.name == "nt" else "",
           script_name
         )
 
