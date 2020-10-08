5434d0d270cc82f05b6352afe3d4f11f44494db4
Ana Mileva <anamileva@gmail.com>
2020-10-08 14:26:03 -0700

    Fix executable path on Windows

--- ui/server/common_functions.py
+++ ui/server/common_functions.py
@@ -21,14 +21,10 @@ def get_executable_path(script_name):
     :param script_name: string; the name of the GridPath script
     :return: string; the path to the GridPath executable
 
-    Get the entry point script path from the sys.executable. On Windows,
-    the GridPath scripts are located in a 'Scripts' directory within the base
-    directory of the Python executable; on Mac, the Python executable and
-    the GridPath script are in the same directory.
+    Get the entry point script path from the sys.executable.
     """
     gridpath_executable = os.path.join(
           os.path.dirname(sys.executable),
-          "Scripts" if os.name == "nt" else "",
           script_name
         )
 
