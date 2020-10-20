0326481037e5b4d8bd97e862dea9ac4847d339dd
Ana Mileva <anamileva@gmail.com>
2020-10-20 14:52:38 -0700

    Fix GP script  path on Windows with Anaconda env

--- ui/server/common_functions.py
+++ ui/server/common_functions.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import glob
 import os.path
 import sys
 
@@ -21,11 +22,28 @@ def get_executable_path(script_name):
     :param script_name: string; the name of the GridPath script
     :return: string; the path to the GridPath executable
 
-    Get the entry point script path from the sys.executable.
+    Get the entry point script path from the sys.executable. In Windows
+    Anaconda environment, the Python executable and entry point scripts are
+    not in the same directory; rather, the entry point scripts are in a
+    directory called 'Scripts' inside the directory where the Python
+    executable is located.
     """
     gridpath_executable = os.path.join(
-          os.path.dirname(sys.executable),
-          script_name
+        os.path.dirname(sys.executable), script_name
+    )
+
+    # If we can't find the GP script in the same directory as the system
+    # executable, we'll try looking for it inside a 'Scripts' directory
+    # This is where it is on Windows with an Anaconda environment
+    if not glob.glob("{}.*".format(gridpath_executable)):
+        gridpath_executable = os.path.join(
+            os.path.dirname(sys.executable), "Scripts", script_name
         )
+        # If we still can't find it, raise an error
+        if not glob.glob("{}.*".format(gridpath_executable)):
+            raise OSError(
+                "ERROR! {} is not the correct path for GridPath executable {}."
+                .format(gridpath_executable, script_name)
+            )
 
     return gridpath_executable
