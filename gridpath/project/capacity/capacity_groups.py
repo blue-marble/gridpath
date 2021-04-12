blue-marble/gridpath

commit dc1fd7f33620e8ddae006b02d1b109960018c9a8

Standard capacity type methods (#790)
Similar to what we did for operational types, capacity types now have 
default methods, so a type method only needs to be specified in the 
capacity type module if it differs from the default.

gridpath/project/capacity/capacity_groups.py
@@ -25,8 +25,9 @@ from pyomo.environ import Set, Param, Constraint, NonNegativeReals, \
 from db.common_functions import spin_on_database_lock
 from gridpath.auxiliary.auxiliary import get_required_subtype_modules_from_projects_file
 from gridpath.project.capacity.common_functions import \
-    load_gen_storage_capacity_type_modules
+    load_project_capacity_type_modules
 from gridpath.auxiliary.db_interface import setup_results_import
+import gridpath.project.capacity.capacity_types as cap_type_init
 
 
 def add_model_components(m, d, scenario_directory, subproblem, stage):
@@ -168,27 +169,36 @@ def add_model_components(m, d, scenario_directory, subproblem, stage):
         stage=stage, which_type="capacity_type"
     )
 
-    imported_capacity_modules = load_gen_storage_capacity_type_modules(
+    imported_capacity_modules = load_project_capacity_type_modules(
         required_capacity_modules
     )
 
     # Get the new and total capacity in the group for the respective
     # expressions
     def new_capacity_rule(mod, prj, prd):
-        gen_cap_type = mod.capacity_type[prj]
+        cap_type = mod.capacity_type[prj]
         # The capacity type modules check if this period is a "vintage" for
         # this project and return 0 if not
-        return imported_capacity_modules[gen_cap_type].new_capacity_rule(
-            mod, prj, prd)
+        if hasattr(imported_capacity_modules[cap_type],
+                   "new_capacity_rule"):
+            return imported_capacity_modules[cap_type]. \
+                new_capacity_rule(mod, prj, prd)
+        else:
+            return cap_type_init.new_capacity_rule(mod, prj, prd)
 
     def total_capacity_rule(mod, prj, prd):
-        gen_cap_type = mod.capacity_type[prj]
+        cap_type = mod.capacity_type[prj]
         # Return the capacity type's capacity rule if the project is
         # operational in this timepoint; otherwise, return 0
-        return imported_capacity_modules[gen_cap_type].capacity_rule(
-            mod, prj, prd) \
-            if prd in mod.OPR_PRDS_BY_PRJ[prj] \
-            else 0
+        if prd in mod.OPR_PRDS_BY_PRJ[prj]:
+            return 0
+        else:
+            if hasattr(imported_capacity_modules[cap_type],
+                       "capacity_rule"):
+                return imported_capacity_modules[cap_type]. \
+                    capacity_rule(mod, prj, prd)
+            else:
+                return cap_type_init.capacity_rule(mod, prj, prd)
 
     # Expressions
     def group_new_capacity_rule(mod, grp, prd):

gridpath/auxiliary/dynamic_components.py
@@ -24,8 +24,6 @@ from builtins import object
 # Create global variables for the dynamic component names, so that we can
 # more easily import the correct names into other modules
 capacity_type_operational_period_sets = "capacity_type_operational_period_sets"
-storage_only_capacity_type_operational_period_sets = \
-    "storage_only_capacity_type_operational_period_sets"
 
 headroom_variables = "headroom_variables"
 footroom_variables = "footroom_variables"
@@ -75,8 +73,6 @@ class DynamicComponents(object):
         # If called, the capacity-type modules will populate these lists with
         # the name of the respective set for the capacity type
         setattr(self, capacity_type_operational_period_sets, list())
-        setattr(self, storage_only_capacity_type_operational_period_sets,
-                list())
 
         # PRM cost groups
         setattr(self, prm_cost_group_sets, list())

gridpath/project/capacity/capacity.py
@@ -29,10 +29,10 @@ from pyomo.environ import Set, Expression, value
 from gridpath.auxiliary.auxiliary import get_required_subtype_modules_from_projects_file, \
     join_sets
 from gridpath.project.capacity.common_functions import \
-    load_gen_storage_capacity_type_modules
+    load_project_capacity_type_modules
 from gridpath.auxiliary.dynamic_components import \
-    capacity_type_operational_period_sets, \
-    storage_only_capacity_type_operational_period_sets
+    capacity_type_operational_period_sets
+import gridpath.project.capacity.capacity_types as cap_type_init
 
 
 def add_model_components(m, d, scenario_directory, subproblem, stage):
@@ -57,13 +57,6 @@ def add_model_components(m, d, scenario_directory, subproblem, stage):
     | can be build). This set is created by joining sets added by the         |
     | capacity_type modules (which is done before loading this module),       |
     | as how operational periods are determined differs by capacity type.     |
-    +-------------------------------------------------------------------------+
-    | | :code:`STOR_OPR_PRDS`                                                 |
-    | | *Within*: :code:`PRJ_OPR_PRDS`                                        |
-    |                                                                         |
-    | Two-dimensional set that defines all project-period combinations when a |
-    | when a storage projects can be operational, i.e. either has specified   |
-    | capacity or can be built).                                              |
     +-------------------------------------------------------------------------+
     | | :code:`OPR_PRDS_BY_PRJ`                                               |
     | | *Defined over*: :code:`PROJECTS`                                      |
@@ -101,11 +94,11 @@ def add_model_components(m, d, scenario_directory, subproblem, stage):
     | Capacity_MW.                                                            |
     +-------------------------------------------------------------------------+
     | | :code:`Energy_Capacity_MWh`                                           |
-    | | *Defined over*: :code:`STOR_OPR_PRDS`                                 |
+    | | *Defined over*: :code:`PRJ_OPR_PRDS`                                  |
     |                                                                         |
-    | Defines the storage project's energy capacity in each period (in which  |
-    | the project can exist). The exact formulation of the expression depends |
-    | on the project's capacity_type. For each project, we call its           |
+    | Defines the project's energy capacity in each period (in which the      |
+    | project can exist). The exact formulation of the expression depends on  |
+    | the project's capacity_type. For each project, we call its              |
     | capacity_type module's energy_capacity_rule method in order to          |
     | formulate the expression.                                               |
     +-------------------------------------------------------------------------+
@@ -121,7 +114,7 @@ def add_model_components(m, d, scenario_directory, subproblem, stage):
     )
 
     # Import needed capacity type modules
-    imported_capacity_modules = load_gen_storage_capacity_type_modules(
+    imported_capacity_modules = load_project_capacity_type_modules(
         required_capacity_modules
     )
 
@@ -142,14 +135,6 @@ def add_model_components(m, d, scenario_directory, subproblem, stage):
         initialize=lambda mod:
         join_sets(mod, getattr(d, capacity_type_operational_period_sets),),
     )  # assumes capacity types model components are already added!
-
-    m.STOR_OPR_PRDS = Set(
-        dimen=2,
-        within=m.PRJ_OPR_PRDS,
-        initialize=lambda mod:
-        join_sets(mod, getattr(
-            d, storage_only_capacity_type_operational_period_sets)),
-    )  # assumes storage capacity type model components are already added!
 
     m.OPR_PRDS_BY_PRJ = Set(
         m.PROJECTS,
@@ -177,30 +162,31 @@ def add_model_components(m, d, scenario_directory, subproblem, stage):
     # Expressions
     ###########################################################################
 
-    def capacity_rule(mod, g, p):
-        gen_cap_type = mod.capacity_type[g]
-        return imported_capacity_modules[gen_cap_type].capacity_rule(mod, g, p)
+    def capacity_rule(mod, prj, prd):
+        cap_type = mod.capacity_type[prj]
+        if hasattr(imported_capacity_modules[cap_type],
+                   "capacity_rule"):
+            return imported_capacity_modules[cap_type]. \
+                capacity_rule(mod, prj, prd)
+        else:
+            return cap_type_init.capacity_rule(mod, prj, prd)
 
     m.Capacity_MW = Expression(
         m.PRJ_OPR_PRDS,
         rule=capacity_rule
     )
 
-    def energy_capacity_rule(mod, g, p):
-        cap_type = mod.capacity_type[g]
-        if hasattr(imported_capacity_modules[cap_type], "energy_capacity_rule"):
+    def energy_capacity_rule(mod, prj, prd):
+        cap_type = mod.capacity_type[prj]
+        if hasattr(imported_capacity_modules[cap_type],
+                   "energy_capacity_rule"):
             return imported_capacity_modules[cap_type]. \
-                energy_capacity_rule(mod, g, p)
+                energy_capacity_rule(mod, prj, prd)
         else:
-            raise Exception("Project " + str(g)
-                            + " is of capacity type " + str(cap_type)
-                            + ". This capacity type module does not have "
-                            + "a function 'energy_capacity_rule,' "
-                            + "but " + str(g)
-                            + " is defined as storage project.")
+            return cap_type_init.energy_capacity_rule(mod, prj, prd)
 
     m.Energy_Capacity_MWh = Expression(
-        m.STOR_OPR_PRDS,
+        m.PRJ_OPR_PRDS,
         rule=energy_capacity_rule
     )
 
@@ -245,7 +231,7 @@ def load_model_data(m, d, data_portal, scenario_directory, subproblem, stage):
     )
 
     # Import needed capacity type modules
-    imported_capacity_modules = load_gen_storage_capacity_type_modules(
+    imported_capacity_modules = load_project_capacity_type_modules(
         required_capacity_modules
     )
     for op_m in required_capacity_modules:
@@ -285,7 +271,6 @@ def export_results(scenario_directory, subproblem, stage, m, d):
                 m.load_zone[prj],
                 value(m.Capacity_MW[prj, p]),
                 value(m.Energy_Capacity_MWh[prj, p])
-                if (prj, p) in m.STOR_OPR_PRDS else None
             ])
 
     # Module-specific capacity results
@@ -295,7 +280,7 @@ def export_results(scenario_directory, subproblem, stage, m, d):
     )
 
     # Import needed capacity type modules
-    imported_capacity_modules = load_gen_storage_capacity_type_modules(
+    imported_capacity_modules = load_project_capacity_type_modules(
         required_capacity_modules
     )
     for op_m in required_capacity_modules:
@@ -344,7 +329,7 @@ def summarize_results(scenario_directory, subproblem, stage):
     )
 
     # Import needed capacity type modules
-    imported_capacity_modules = load_gen_storage_capacity_type_modules(
+    imported_capacity_modules = load_project_capacity_type_modules(
         required_capacity_modules
     )
     for op_m in required_capacity_modules:

gridpath/project/capacity/capacity_types/__init__.py
@@ -23,7 +23,7 @@ import os.path
 
 from db.common_functions import spin_on_database_lock
 from gridpath.project.capacity.common_functions import \
-    load_gen_storage_capacity_type_modules
+    load_project_capacity_type_modules
 from gridpath.auxiliary.db_interface import \
     get_required_capacity_types_from_database, setup_results_import
 
@@ -41,7 +41,7 @@ def validate_inputs(scenario_id, subscenarios, subproblem, stage, conn):
     # Load in the required capacity type modules
     required_capacity_type_modules = \
         get_required_capacity_types_from_database(conn, scenario_id,)
-    imported_capacity_type_modules = load_gen_storage_capacity_type_modules(
+    imported_capacity_type_modules = load_project_capacity_type_modules(
         required_capacity_type_modules)
 
     # Validate module-specific inputs
@@ -70,7 +70,7 @@ def write_model_inputs(scenario_directory, scenario_id, subscenarios, subproblem
 
     required_capacity_type_modules = \
         get_required_capacity_types_from_database(conn, scenario_id)
-    imported_capacity_type_modules = load_gen_storage_capacity_type_modules(
+    imported_capacity_type_modules = load_project_capacity_type_modules(
         required_capacity_type_modules)
 
     # Get module-specific inputs
@@ -158,7 +158,7 @@ def import_results_into_database(
     # Load in the required capacity type modules
     required_capacity_type_modules = \
         get_required_capacity_types_from_database(db, scenario_id)
-    imported_capacity_type_modules = load_gen_storage_capacity_type_modules(
+    imported_capacity_type_modules = load_project_capacity_type_modules(
         required_capacity_type_modules)
 
     # Import module-specific results
@@ -171,3 +171,29 @@ def import_results_into_database(
             )
         else:
             pass
+
+# Capacity Type Module Method Defaults
+###############################################################################
+def capacity_rule(mod, g, p):
+    """
+    """
+    return 0
+
+
+def energy_capacity_rule(mod, g, p):
+    """
+    """
+    return 0
+
+
+def capacity_cost_rule(mod, g, p):
+    """
+    """
+    return 0
+
+
+def new_capacity_rule(mod, g, p):
+    """
+    New capacity built at project g in period p.
+    """
+    return 0

gridpath/project/capacity/capacity_types/dr_new.py
@@ -44,8 +44,7 @@ from db.common_functions import spin_on_database_lock
 from gridpath.auxiliary.auxiliary import cursor_to_df
 from gridpath.auxiliary.db_interface import setup_results_import
 from gridpath.auxiliary.dynamic_components import \
-    capacity_type_operational_period_sets, \
-    storage_only_capacity_type_operational_period_sets
+    capacity_type_operational_period_sets
 from gridpath.auxiliary.validations import write_validation_to_database, \
     validate_missing_inputs, validate_idxs, get_projects
 
@@ -252,12 +251,6 @@ def add_model_components(m, d, scenario_directory, subproblem, stage):
     # Add to list of sets we'll join to get the final
     # PRJ_OPR_PRDS set
     getattr(d, capacity_type_operational_period_sets).append(
-        "DR_NEW_OPR_PRDS",
-    )
-    # Add to list of sets we'll join to get the final
-    # STOR_OPR_PRDS set
-    # We'll include shiftable load with storage
-    getattr(d, storage_only_capacity_type_operational_period_sets).append(
         "DR_NEW_OPR_PRDS",
     )
 

gridpath/project/capacity/capacity_types/gen_ret_bin.py
@@ -227,13 +227,6 @@ def capacity_cost_rule(mod, g, p):
         * (1 - mod.GenRetBin_Retire[g, p])
 
 
-def new_capacity_rule(mod, g, p):
-    """
-    New capacity built at project g in period p.
-    """
-    return 0
-
-
 # Input-Output
 ###############################################################################
 

gridpath/project/capacity/capacity_types/gen_ret_lin.py
@@ -265,13 +265,6 @@ def capacity_cost_rule(mod, g, p):
         * mod.gen_ret_lin_fixed_cost_per_mw_yr[g, p]
 
 
-def new_capacity_rule(mod, g, p):
-    """
-    New capacity built at project g in period p.
-    """
-    return 0
-
-
 # Input-Output
 ###############################################################################
 

gridpath/project/capacity/capacity_types/gen_spec.py
@@ -127,13 +127,6 @@ def capacity_cost_rule(mod, g, p):
         * mod.gen_spec_fixed_cost_per_mw_yr[g, p]
 
 
-def new_capacity_rule(mod, g, p):
-    """
-    New capacity built at project g in period p.
-    """
-    return 0
-
-
 # Input-Output
 ###############################################################################
 

commit f34a866c2eee3526576f00b35e047f5c5ed73e1f

Fix capacity groups bug introduced in #790

gridpath/project/capacity/capacity_groups.py
@@ -190,7 +190,7 @@ def add_model_components(m, d, scenario_directory, subproblem, stage):
         cap_type = mod.capacity_type[prj]
         # Return the capacity type's capacity rule if the project is
         # operational in this timepoint; otherwise, return 0
-        if prd in mod.OPR_PRDS_BY_PRJ[prj]:
+        if prd not in mod.OPR_PRDS_BY_PRJ[prj]:
             return 0
         else:
             if hasattr(imported_capacity_modules[cap_type],