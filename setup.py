f12a3ffe6364b6a61805d0ad466a51dc5284b39d
Ana Mileva <anamileva@users.noreply.github.com>
2022-01-06 12:39:20 -0800

    Update requirements

--- setup.py
+++ setup.py
@@ -7,57 +7,65 @@ with open("./version.py") as fp:
 
 # Set up extras
 extras_doc = [
-    "Sphinx==3.3.1",
-    "sphinx-argparse==0.2.5"
+    "Sphinx==4.0.2",
+    "sphinx-argparse==0.2.5",
+    "numpy==1.21.5"  # temporarily require v1.21.5 because v1.22 is not available on
+    # readthedocs and build fails otherwise
 ]
 extras_ui = [
-    "eventlet==0.29.1",  # Async mode for SocketIO
-    "Flask==1.1.2",  # Local API server for UI
-    "Flask-RESTful==0.3.8",  # Flask extension for building REST APIs
-    "Flask-SocketIO==4.3.1",  # Flask client-server communication
-    "psutil==5.7.3",  # Process management
-    "python-socketio[client]==4.6.0",  # SocketIO Python client
+    "eventlet==0.31.0",  # Async mode for SocketIO
+    "Flask==2.0.1",  # Local API server for UI
+    "Flask-RESTful==0.3.9",  # Flask extension for building REST APIs
+    "Flask-SocketIO==4.3.2",  # Flask client-server communication
+    "psutil==5.8.0",  # Process management
+    "python-socketio[client]<5,>=4.3.0",  # SocketIO Python client
 ]
-extras_all = extras_ui + extras_doc
+extras_black = ["black"]
 
+extras_coverage = [
+    "coverage",  # test coverage
+    "coveralls",  # automated coverage results
+]
+extras_all = extras_ui + extras_doc + extras_black + extras_coverage
 
-setup(name="GridPath",
-      version=version["__version__"],
-      description="Software for power-system planning",
-      url="https://www.gridpath.io",
-      maintainer="Blue Marble Analytics LLC",
-      maintainer_email="info@gridpath.io",
-      license="TBD",
-      platforms=["any"],
-      keywords=[
-          "energy", "electricity", "power", "renewables",
-          "planning", "operations"
-      ],
-      packages=find_packages(),
-      install_requires=[
-          "Pyomo==5.7.1",  # Optimization modeling language
-          "pandas==1.1.4",  # Data-processing
-          "bokeh==2.2.3",  # Visualization library
-          "pscript==0.7.4",  # Python to JavaScript compiler (for visualization)
-          "networkx==2.5"  # network package for DC OPF
-      ],
-      extras_require={
-          "doc": extras_doc,
-          "ui": extras_ui,
-          "all": extras_all
-      },
-      include_package_data=True,
-      entry_points={
-          "console_scripts": [
-              "gridpath_run = gridpath.run_scenario:main",
-              "gridpath_run_e2e = gridpath.run_end_to_end:main",
-              "gridpath_get_inputs = gridpath.get_scenario_inputs:main",
-              "gridpath_import_results = "
-              "gridpath.import_scenario_results:main",
-              "gridpath_process_results = gridpath.process_results:main",
-              "gridpath_validate = gridpath.validate_inputs:main",
-              "gridpath_run_server = ui.server.run_server:main",
-              "gridpath_run_queue_manager = ui.server.run_queue_manager:main"
-          ]
-      }
-      )
+setup(
+    name="GridPath",
+    version=version["__version__"],
+    description="A versatile simulation and optimization platform for "
+    "power-system planning and operations.",
+    url="https://www.gridpath.io",
+    maintainer="Blue Marble Analytics LLC",
+    maintainer_email="info@gridpath.io",
+    license="Apache v2",
+    platforms=["MacOS", "Windows"],
+    keywords=["energy", "electricity", "power", "renewables", "planning", "operations"],
+    packages=find_packages(),
+    install_requires=[
+        "Pyomo==5.7.3",  # Optimization modeling language (required due to
+        # Pyomo/Cbc issue with UI on Mac)
+        "pandas==1.2.5",  # Data-processing
+        "bokeh==2.2.3",  # Visualization library (required - see #779)
+        "pscript==0.7.5",  # Python to JavaScript compiler (for viz)
+        "networkx==2.5.1",  # network package for DC OPF
+        "pyutilib==6.0.0",  # used for solver temp file management
+    ],
+    extras_require={
+        "doc": extras_doc,
+        "ui": extras_ui,
+        "all": extras_all,
+        "coverage": extras_coverage,
+    },
+    include_package_data=True,
+    entry_points={
+        "console_scripts": [
+            "gridpath_run = gridpath.run_scenario:main",
+            "gridpath_run_e2e = gridpath.run_end_to_end:main",
+            "gridpath_get_inputs = gridpath.get_scenario_inputs:main",
+            "gridpath_import_results = " "gridpath.import_scenario_results:main",
+            "gridpath_process_results = gridpath.process_results:main",
+            "gridpath_validate = gridpath.validate_inputs:main",
+            "gridpath_run_server = ui.server.run_server:main",
+            "gridpath_run_queue_manager = ui.server.run_queue_manager:main",
+        ]
+    },
+)
