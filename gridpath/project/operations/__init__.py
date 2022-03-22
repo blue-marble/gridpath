c677da59d38af2131f4c5c08b3307b96b46759b8
Ana Mileva <anamileva@users.noreply.github.com>
2022-03-22 10:37:23 -0700

    Support for cycle selection  (#882)
    Implement a joint constraint on sync decisions for multiple projects,
    e.g. for simple cycle vs combined cycle mode.

--- gridpath/project/operations/__init__.py
+++ gridpath/project/operations/__init__.py
@@ -817,7 +817,29 @@ def get_inputs_from_database(scenario_id, subscenarios, subproblem, stage, conn)
         )
     )
 
-    return proj_opchar, fuels, heat_rates, vom_curves, startup_chars
+    c6 = conn.cursor()
+    cycle_selection = c6.execute(
+        """
+        SELECT project, cycle_selection_project
+        FROM inputs_project_portfolios
+        INNER JOIN
+        (SELECT project, cycle_selection_scenario_id
+        FROM inputs_project_operational_chars
+        WHERE project_operational_chars_scenario_id = {project_opchar_scenario_id}
+        ) AS op_char
+        USING (project)
+        INNER JOIN
+        inputs_project_cycle_selection
+        USING(project, cycle_selection_scenario_id)
+        WHERE project_portfolio_scenario_id = {project_portfolio_scenario_id}
+        AND cycle_selection_scenario_id IS NOT NULL
+        """.format(
+            project_opchar_scenario_id=subscenarios.PROJECT_OPERATIONAL_CHARS_SCENARIO_ID,
+            project_portfolio_scenario_id=subscenarios.PROJECT_PORTFOLIO_SCENARIO_ID,
+        )
+    )
+
+    return proj_opchar, fuels, heat_rates, vom_curves, startup_chars, cycle_selection
 
 
 def write_model_inputs(
@@ -838,7 +860,12 @@ def write_model_inputs(
         heat_rate_curves,
         vom_curves,
         startup_chars,
+        cycle_selection,
     ) = get_inputs_from_database(scenario_id, subscenarios, subproblem, stage, conn)
+
+    inputs_directory = os.path.join(
+        scenario_directory, str(subproblem), str(stage), "inputs"
+    )
 
     # Update the projects.tab file
     new_columns = [
@@ -870,69 +897,50 @@ def write_model_inputs(
         "curtailment_cost_per_pwh",
     ]
     append_to_input_file(
-        inputs_directory=os.path.join(
-            scenario_directory, str(subproblem), str(stage), "inputs"
-        ),
+        inputs_directory=inputs_directory,
         input_file="projects.tab",
         query_results=proj_opchar,
         index_n_columns=1,
         new_column_names=new_columns,
     )
 
-    # TODO: refactor these inputs, we're doing the same thing
     # Write fuels file
     fuels_df = cursor_to_df(fuels)
-    if not fuels_df.empty:
-        fuels_df = fuels_df.fillna(".")
-        fpath = os.path.join(
-            scenario_directory,
-            str(subproblem),
-            str(stage),
-            "inputs",
-            "project_fuels.tab",
-        )
-
-        fuels_df.to_csv(fpath, index=False, sep="\t")
+    write_additional_opchar_file(
+        opchar_df=fuels_df,
+        inputs_directory=inputs_directory,
+        filename="project_fuels.tab",
+    )
 
     # Write heat rates file
     hr_df = cursor_to_df(heat_rate_curves)
-    if not hr_df.empty:
-        hr_df = hr_df.fillna(".")
-        fpath = os.path.join(
-            scenario_directory,
-            str(subproblem),
-            str(stage),
-            "inputs",
-            "heat_rate_curves.tab",
-        )
-
-        hr_df.to_csv(fpath, index=False, sep="\t")
+    write_additional_opchar_file(
+        opchar_df=hr_df,
+        inputs_directory=inputs_directory,
+        filename="heat_rate_curves.tab",
+    )
 
     # Write VOM file
     vom_df = cursor_to_df(vom_curves)
-    if not vom_df.empty:
-        vom_df = vom_df.fillna(".")
-        fpath = os.path.join(
-            scenario_directory,
-            str(subproblem),
-            str(stage),
-            "inputs",
-            "variable_om_curves.tab",
-        )
-        vom_df.to_csv(fpath, index=False, sep="\t")
+    write_additional_opchar_file(
+        opchar_df=vom_df,
+        inputs_directory=inputs_directory,
+        filename="variable_om_curves.tab",
+    )
 
     # Write startup chars file
     su_df = cursor_to_df(startup_chars)
-    if not su_df.empty:
-        su_df = su_df.fillna(".")
-        fpath = os.path.join(
-            scenario_directory,
-            str(subproblem),
-            str(stage),
-            "inputs",
-            "startup_chars.tab",
-        )
-        su_df.to_csv(fpath, index=False, sep="\t")
+    write_additional_opchar_file(
+        opchar_df=su_df, inputs_directory=inputs_directory, filename="startup_chars.tab"
+    )
+
+    # Write the cycle selection file
+    cs_df = cursor_to_df(cycle_selection)
+    write_additional_opchar_file(
+        opchar_df=cs_df,
+        inputs_directory=inputs_directory,
+        filename="cycle_selection.tab",
+    )
 
 
 # Validation
@@ -956,6 +964,7 @@ def validate_inputs(scenario_id, subscenarios, subproblem, stage, conn):
         heat_rates,
         vom_curves,
         startup_chars,
+        cycle_select,
     ) = get_inputs_from_database(scenario_id, subscenarios, subproblem, stage, conn)
 
     # Convert input data into DataFrame
@@ -1381,3 +1390,14 @@ def calculate_slope_intercept(project, load_points, heat_rates):
             )
 
     return slopes, intercepts
+
+
+def write_additional_opchar_file(opchar_df, inputs_directory, filename):
+    """
+    Write input tab file to the multi-dimensional operating characterstics from a
+    dataframe.
+    """
+    if not opchar_df.empty:
+        opchar_df = opchar_df.fillna(".")
+        fpath = os.path.join(inputs_directory, filename)
+        opchar_df.to_csv(fpath, index=False, sep="\t")
