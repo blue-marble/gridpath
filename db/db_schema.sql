df1a096a92952edd4d97939251148ff6d9afa40e
Ana Mileva <anamileva@users.noreply.github.com>
2022-12-12 10:41:11 -0800

    Add limits on total USE & max unserved load

--- db/db_schema.sql
+++ db/db_schema.sql
@@ -388,7 +388,9 @@ allow_overgeneration INTEGER,
 overgeneration_penalty_per_mw FLOAT,
 allow_unserved_energy INTEGER,
 unserved_energy_penalty_per_mwh FLOAT,
+unserved_energy_limit_mwh FLOAT,  -- limit on the total USE
 max_unserved_load_penalty_per_mw FLOAT,
+max_unserved_load_limit_mw FLOAT, -- limit on the max unserved load
 export_penalty_cost_per_mwh FLOAT,
 PRIMARY KEY (load_zone_scenario_id, load_zone),
 FOREIGN KEY (load_zone_scenario_id) REFERENCES
