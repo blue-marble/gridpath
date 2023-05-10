e1d8f2daf3caea4ef1b0e6c450d2b6b9db413f99
Ana Mileva <anamileva@users.noreply.github.com>
2023-05-10 16:14:37 -0700

    Rename CSV structure file

--- db/utilities/common_functions.py
+++ db/utilities/common_functions.py
@@ -521,17 +521,17 @@ def generic_insert_subscenario(
         )
 
 
-def determine_tables_to_delete_from(csv_data_master, subscenario):
+def determine_tables_to_delete_from(csv_structure, subscenario):
     """
-    :param csv_data_master: Pandas DataFrame
+    :param csv_structure: Pandas DataFrame
     :param subscenario: str;
     :return: subscenario_table, input_tables, project_flag, project_is_tx,
         base_table, base_subscenario
 
     Determine the relevant tables from which to delete prior data.
     """
-    # Get the sub-dataframe for this subscenario from the master CSV
-    subscenario_df = csv_data_master.loc[csv_data_master["subscenario"] == subscenario]
+    # Get the sub-dataframe for this subscenario from the CSV structure file
+    subscenario_df = csv_structure.loc[csv_structure["subscenario"] == subscenario]
 
     # Determine the relevant tables for this subscenario
     subscenario_table = None
