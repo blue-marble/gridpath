cbeb968cf5310e75dfba62e1e664ad5c986e4661
Ana Mileva <anamileva@users.noreply.github.com>
2023-12-12 11:01:23 -0800

    Fix NULL spinup_and_lookahead flag in results

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -1272,6 +1272,17 @@ class TestExamples(unittest.TestCase):
             -3592014778836.2856,
         )
 
+    def test_example_single_stage_prod_cost_w_spinup_lookahead(self):
+        """
+        Check validation and objective function values of
+        "single_stage_prod_cost_w_spinup_lookahead" example
+        :return:
+        """
+        scenario_name = "single_stage_prod_cost_w_spinup_lookahead"
+        self.validate_and_test_example_generic(
+            scenario_name=scenario_name, literal=True
+        )
+
     @classmethod
     def tearDownClass(cls):
         os.remove(DB_PATH)
