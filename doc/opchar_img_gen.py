da8b6815e8249013a8614d11da8ec8af1d0b05bf
Milind <milind.r.nath@gmail.com>
2024-03-12 13:46:10 +0530

    Added license and docstring

--- doc/opchar_img_gen.py
+++ doc/opchar_img_gen.py
@@ -1,3 +1,24 @@
+# Copyright 2016-2023 Blue Marble Analytics LLC.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+This script generates the images (SVG and PNG) of the table of operational 
+characteristics with the corresponding project operational types for 
+inclusion into the documentation.
+
+"""
+
 import pandas as pd, numpy as np
 import df2img
 
