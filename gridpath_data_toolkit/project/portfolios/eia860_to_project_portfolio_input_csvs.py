df816d39820e5cdc8036d1c2ccf9038ce57bb8a3
Ana Mileva <anamileva@users.noreply.github.com>
2024-08-29 14:08:18 -0700

    Move data toolkit to root-level package

---
+++ gridpath_data_toolkit/project/portfolios/eia860_to_project_portfolio_input_csvs.py
@@ -0,0 +1,162 @@
+# Copyright 2016-2024 Blue Marble Analytics LLC.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Create project portfolios from EIA860 data.
+
+The project capacity_types will be based on the data in the
+user_defined_eia_gridpath_key table.
+
+Wind, solar, and hydro are aggregated to the BA level.
+
+.. note:: Hybrids are currently not treated separately. Their renewable
+generation components are lumped with wind/solar, and the storage components
+show up as individual units.
+
+Project portfolios are created based on the data from a particula report
+date. The user selects the region (determines subset of generators to use)
+and the study date (determines which generators are operational, i.e.,
+after their online date and before their retirement date in the EIA data.)
+
+TODO: disaggregate the hybrids out of the wind/solar project and combine
+     with their battery components
+"""
+
+from argparse import ArgumentParser
+import os.path
+import pandas as pd
+import sys
+
+from db.common_functions import connect_to_database
+from gridpath_data_toolkit.project.project_data_filters_common import (
+    get_eia860_sql_filter_string,
+    VAR_GEN_FILTER_STR,
+    HYDRO_FILTER_STR,
+    DISAGG_PROJECT_NAME_STR,
+    AGG_PROJECT_NAME_STR,
+)
+
+
+def parse_arguments(args):
+    """
+    :param args: the script arguments specified by the user
+    :return: the parsed known argument values (<class 'argparse.Namespace'>
+    Python object)
+
+    Parse the known arguments.
+    """
+    parser = ArgumentParser(add_help=True)
+
+    parser.add_argument("-db", "--database", default="../../open_data.db")
+    parser.add_argument("-rep", "--report_date", default="2023-01-01")
+    parser.add_argument("-y", "--study_year", default=2026)
+    parser.add_argument("-r", "--region", default="WECC")
+    parser.add_argument(
+        "-o",
+        "--output_directory",
+        default="../../csvs_open_data/project/portfolios",
+    )
+    parser.add_argument("-p_id", "--project_portfolio_scenario_id", default=1)
+    parser.add_argument(
+        "-p_name", "--project_portfolio_scenario_name", default="wecc_plants_units"
+    )
+
+    parsed_arguments = parser.parse_known_args(args=args)[0]
+
+    return parsed_arguments
+
+
+def get_project_portfolio_for_region(
+    conn,
+    eia860_sql_filter_string,
+    var_gen_filter_str,
+    hydro_filter_str,
+    disagg_project_name_str,
+    agg_project_name_str,
+    csv_location,
+    subscenario_id,
+    subscenario_name,
+):
+    """
+    Unit level except for wind (onshore and offshore) and solar PV, which are
+    aggregated to the BA-level.
+    """
+    # For disaggregated unit-level projects, use plant_id_eia__generator_id
+    # as the project name
+    # For BA-aggregated projects, use prime_mover_BA
+    sql = f"""
+    -- Disaggregated units
+    SELECT {disagg_project_name_str} AS project, 
+    NULL as specified, 
+    NULL as new_build,
+    gridpath_capacity_type AS capacity_type
+    FROM raw_data_eia860_generators
+    JOIN user_defined_eia_gridpath_key ON
+            raw_data_eia860_generators.prime_mover_code = 
+            user_defined_eia_gridpath_key.prime_mover_code
+            AND energy_source_code_1 = energy_source_code
+     WHERE 1 = 1
+     AND {eia860_sql_filter_string}
+     AND NOT {var_gen_filter_str}
+     AND NOT {hydro_filter_str}
+    UNION
+    -- Aggregated units include wind, offshore wind, solar, and hydro
+    SELECT {agg_project_name_str} AS project,
+        NULL as specified,
+        NULL as new_build,
+        gridpath_capacity_type AS capacity_type
+    FROM raw_data_eia860_generators
+    JOIN user_defined_eia_gridpath_key
+    USING (prime_mover_code)
+    WHERE 1 = 1
+    AND {eia860_sql_filter_string}
+    AND ({var_gen_filter_str} OR {hydro_filter_str})
+    ;
+    """
+
+    df = pd.read_sql(sql, conn)
+    df.to_csv(
+        os.path.join(csv_location, f"{subscenario_id}_{subscenario_name}.csv"),
+        index=False,
+    )
+
+
+def main(args=None):
+    print("Creating project portfolio inputs")
+    if args is None:
+        args = sys.argv[1:]
+
+    parsed_args = parse_arguments(args=args)
+
+    os.makedirs(parsed_args.output_directory, exist_ok=True)
+
+    conn = connect_to_database(db_path=parsed_args.database)
+
+    get_project_portfolio_for_region(
+        conn=conn,
+        eia860_sql_filter_string=get_eia860_sql_filter_string(
+            study_year=parsed_args.study_year, region=parsed_args.region
+        ),
+        var_gen_filter_str=VAR_GEN_FILTER_STR,
+        hydro_filter_str=HYDRO_FILTER_STR,
+        disagg_project_name_str=DISAGG_PROJECT_NAME_STR,
+        agg_project_name_str=AGG_PROJECT_NAME_STR,
+        csv_location=parsed_args.output_directory,
+        subscenario_id=parsed_args.project_portfolio_scenario_id,
+        subscenario_name=parsed_args.project_portfolio_scenario_name,
+    )
+
+
+if __name__ == "__main__":
+    main()
---
+++ gridpath_data_toolkit/transmission/capacity_specified/create_transmission_specified_capacity_input_csvs.py
@@ -0,0 +1,174 @@
+# Copyright 2016-2024 Blue Marble Analytics LLC.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+from argparse import ArgumentParser
+import numpy as np
+import os.path
+import pandas as pd
+import sys
+
+from db.common_functions import connect_to_database
+from gridpath_data_toolkit.transmission.transmission_data_filters_common import (
+    get_all_links_sql,
+)
+
+
+def parse_arguments(args):
+    """
+    :param args: the script arguments specified by the user
+    :return: the parsed known argument values (<class 'argparse.Namespace'>
+    Python object)
+
+    Parse the known arguments.
+    """
+    parser = ArgumentParser(add_help=True)
+
+    parser.add_argument("-db", "--database", default="../../open_data.db")
+    parser.add_argument("-rep", "--report_date", default="2023-01-01")
+    parser.add_argument("-y", "--study_year", default=2026)
+    parser.add_argument("-r", "--region", default="WECC")
+
+    parser.add_argument(
+        "-cap_csv",
+        "--specified_capacity_csv_location",
+        default="../../csvs_open_data/transmission/capacity_specified",
+    )
+    parser.add_argument(
+        "-cap_id", "--transmission_specified_capacity_scenario_id", default=1
+    )
+    parser.add_argument(
+        "-cap_name", "--transmission_specified_capacity_scenario_name", default="base"
+    )
+
+    parsed_arguments = parser.parse_known_args(args=args)[0]
+
+    return parsed_arguments
+
+
+def get_tx_capacities(
+    conn,
+    all_links,
+    period,
+    cap,
+    threshold,
+    csv_location,
+    subscenario_id,
+    subscenario_name,
+):
+    df_list = []
+    from_to_combos = []
+    for lz_from, lz_to in all_links:
+        if (lz_from, lz_to) not in from_to_combos:
+            # Exclude values abs(x) >= cap
+            # TODO: this is based on a manual scan of the data; we need a more robust
+            #  way of cleaning it and detecting outliers
+            min_max_sql = f"""
+                SELECT
+                    CASE WHEN max(i1) IS NULL THEN 0 ELSE max(i1) END,
+                    CASE WHEN min(i1) IS NULL THEN 0 ELSE min(i1) END,
+                    CASE WHEN max(i2) IS NULL THEN 0 ELSE max(i2) END,
+                    CASE WHEN min(i2) IS NULL THEN 0 ELSE min(i2) END
+                FROM (
+                SELECT datetime_pst, from1, to1, i1, from2, to2, i2, i1/i2, i2/i1 FROM (
+                SELECT datetime_pst, balancing_authority_code_eia as from1, balancing_authority_code_adjacent_eia as to1, interchange_reported_mwh as i1
+                FROM raw_data_eia930_hourly_interchange
+                WHERE balancing_authority_code_eia = '{lz_from}' AND 
+                balancing_authority_code_adjacent_eia = '{lz_to}'
+                AND abs(interchange_reported_mwh) <= {cap}
+                ) as tbl1
+                JOIN (
+                SELECT datetime_pst, balancing_authority_code_eia as from2, balancing_authority_code_adjacent_eia as to2, interchange_reported_mwh as i2
+                FROM raw_data_eia930_hourly_interchange
+                WHERE balancing_authority_code_eia = '{lz_to}' 
+                AND balancing_authority_code_adjacent_eia = '{lz_from}'
+                AND abs(interchange_reported_mwh) <= {cap}
+                ) as tbl2
+                USING (datetime_pst)
+                )
+            """
+
+            c = conn.cursor()
+            max_i1, min_i1, max_i2, min_i2 = c.execute(min_max_sql).fetchone()
+
+            # If the same is reported on both sides (within some threshold
+            # percentage), use values directly and take the average
+            if (1 - threshold) * -min_i2 <= max_i1 <= (1 + threshold) * -min_i2 or (
+                1 - threshold
+            ) * max_i1 <= -min_i2 <= (1 + threshold) * max_i1:
+                max_to_use = np.mean([max_i1, -min_i2])
+            else:
+                max_to_use = min([max_i1, -min_i2])
+
+            if (1 - threshold) * max_i2 <= -min_i1 <= (1 + threshold) * max_i2 or (
+                1 - threshold
+            ) * -min_i1 <= max_i2 <= (1 + threshold) * -min_i1:
+                min_to_use = np.mean([-min_i1, max_i2])
+            else:
+                min_to_use = min([-min_i1, max_i2])
+
+            tx_capacity = max([max_to_use, min_to_use])
+
+            df = pd.DataFrame(
+                {
+                    "transmission_line": [f"{lz_from}_{lz_to}"],
+                    "period": [period],
+                    "min_mw": [-tx_capacity],
+                    "max_mw": [tx_capacity],
+                    "fixed_cost_per_mw_yr": [0],
+                }
+            )
+
+            df_list.append(df)
+
+            from_to_combos.append((lz_from, lz_to))
+            from_to_combos.append((lz_to, lz_from))
+
+    # Concat the individual line dataframes for export
+    df = pd.concat(df_list)
+    df.to_csv(
+        os.path.join(csv_location, f"{subscenario_id}_{subscenario_name}.csv"),
+        index=False,
+    )
+
+
+def main(args=None):
+    print("Creating transmission specified capacity inputs")
+    if args is None:
+        args = sys.argv[1:]
+
+    parsed_args = parse_arguments(args=args)
+
+    os.makedirs(parsed_args.specified_capacity_csv_location, exist_ok=True)
+
+    conn = connect_to_database(db_path=parsed_args.database)
+
+    c = conn.cursor()
+
+    all_links = c.execute(get_all_links_sql(region="WECC")).fetchall()
+
+    get_tx_capacities(
+        conn=conn,
+        all_links=all_links,
+        period=2026,
+        cap=19000,
+        threshold=0.5,
+        csv_location=parsed_args.specified_capacity_csv_location,
+        subscenario_id=parsed_args.transmission_specified_capacity_scenario_id,
+        subscenario_name=parsed_args.transmission_specified_capacity_scenario_name,
+    )
+
+
+if __name__ == "__main__":
+    main()
