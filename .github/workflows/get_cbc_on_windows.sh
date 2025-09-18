b023aa42ae9404796e15b039d6384eb789e3670b
Ana Mileva <anamileva@users.noreply.github.com>
2025-09-18 16:20:34 -0700

    one more try

--- .github/workflows/get_cbc_on_windows.sh
+++ .github/workflows/get_cbc_on_windows.sh
@@ -8,4 +8,8 @@ unzip Cbc-releases.2.10.12-w64-msvc17-md.zip -d ./cbc
 # Check if we got it for now
 pwd
 cd cbc/bin
+pwd
 dir
+D:/a/gridpath/gridpath/cbc/bin/cbc.exe --version
+
+echo "D:\a\gridpath\gridpath\cbc\bin" >> $GITHUB_PATH
