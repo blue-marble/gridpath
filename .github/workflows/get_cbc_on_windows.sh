dbd10e328deaaa911ddb4938642fe69eefcd061a
Ana Mileva <anamileva@users.noreply.github.com>
2025-09-18 14:56:25 -0700

    Some options to actually get the file

--- .github/workflows/get_cbc_on_windows.sh
+++ .github/workflows/get_cbc_on_windows.sh
@@ -1,7 +1,10 @@
 # Get the binary build of Cbc solver on Windows
-curl https://github.com/coin-or/Cbc/releases/download/releases%2F2.10.12/Cbc-releases.2.10.12-w64-msvc17-md.zip
+curl -OL https://github.com/coin-or/Cbc/releases/download/releases%2F2.10.12/Cbc-releases.2.10.12-w64-msvc17-md.zip
+# Make directory for Cbc
+mkdir cbc
 # Unzipping binary
-tar -xvzf Cbc-releases.2.10.12-w64-msvc17-md.zip
+tar -xvzf Cbc-releases.2.10.12-w64-msvc17-md.zip -C ./cbc
 
 # Check if we got it for now
+cd ./cbc
 dir
