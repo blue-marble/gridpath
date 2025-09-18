7cfa9aacb7c8b1752fae7dda98c5a186fd9608ff
Ana Mileva <anamileva@users.noreply.github.com>
2025-09-18 15:02:06 -0700

    Try some stuff

--- .github/workflows/get_cbc_on_windows.sh
+++ .github/workflows/get_cbc_on_windows.sh
@@ -3,8 +3,8 @@ curl -OL https://github.com/coin-or/Cbc/releases/download/releases%2F2.10.12/Cbc
 # Make directory for Cbc
 mkdir cbc
 # Unzipping binary
-tar -xvzf Cbc-releases.2.10.12-w64-msvc17-md.zip -C ./cbc
+tar -xvzf Cbc-releases.2.10.12-w64-msvc17-md.zip -C ./cbc --strip-components 1
 
 # Check if we got it for now
-cd ./cbc
+cd cbc
 dir
