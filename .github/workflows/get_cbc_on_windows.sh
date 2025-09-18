9ecdb14a595c1ccbb1a7fa39d3afda96087b3f47
Ana Mileva <anamileva@users.noreply.github.com>
2025-09-18 15:14:43 -0700

    See if unzip works

--- .github/workflows/get_cbc_on_windows.sh
+++ .github/workflows/get_cbc_on_windows.sh
@@ -3,7 +3,7 @@ curl -OL https://github.com/coin-or/Cbc/releases/download/releases%2F2.10.12/Cbc
 # Make directory for Cbc
 mkdir cbc
 # Unzipping binary
-tar -xf Cbc-releases.2.10.12-w64-msvc17-md.zip -C ./cbc --strip-components 1
+unzip Cbc-releases.2.10.12-w64-msvc17-md.zip -d ./cbc
 
 # Check if we got it for now
 cd cbc
