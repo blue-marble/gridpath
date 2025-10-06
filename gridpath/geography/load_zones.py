94b6067f5cd5b482b31d0117380e894f0c546f0d
Ana Mileva <anamileva@users.noreply.github.com>
2025-10-06 10:22:05 -0700

    Separate load zone 'portfolio' from load balance params

--- gridpath/geography/load_zones.py
+++ gridpath/geography/load_zones.py
@@ -136,16 +136,18 @@ def get_inputs_from_database(
 
     c = conn.cursor()
     load_zones = c.execute(
-        """
+        f"""
         SELECT load_zone, allow_overgeneration, overgeneration_penalty_per_mw, 
         allow_unserved_energy, unserved_energy_penalty_per_mwh, 
         unserved_energy_limit_mwh, max_unserved_load_penalty_per_mw, 
         max_unserved_load_limit_mw, export_penalty_cost_per_mwh
-        FROM inputs_geography_load_zones
-        WHERE load_zone_scenario_id = {};
-        """.format(
-            subscenarios.LOAD_ZONE_SCENARIO_ID
-        )
+        FROM inputs_geography_load_balance
+        WHERE load_balance_scenario_id = {subscenarios.LOAD_BALANCE_SCENARIO_ID}
+        AND load_zone in (
+            SELECT load_zone
+            FROM inputs_geography_load_zones        
+            WHERE load_zone_scenario_id = {subscenarios.LOAD_ZONE_SCENARIO_ID}
+        );"""
     )
 
     return load_zones
