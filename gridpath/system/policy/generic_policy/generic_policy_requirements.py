d4cc65f14899e948018c2114bdd54f6b17846755
Ana Mileva <anamileva@users.noreply.github.com>
2026-05-18 13:46:09 -0700

    Allow negative generic policy targets

--- gridpath/system/policy/generic_policy/generic_policy_requirements.py
+++ gridpath/system/policy/generic_policy/generic_policy_requirements.py
@@ -10,7 +10,7 @@
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
+# See the License for the gitspecific language governing permissions and
 # limitations under the License.
 
 """
@@ -20,7 +20,7 @@ Requirements for each policy and policy zone
 import csv
 import os.path
 
-from pyomo.environ import Set, Param, NonNegativeReals, Expression, value, Any
+from pyomo.environ import Set, Param, Reals, NonNegativeReals, Expression, value, Any
 
 from gridpath.auxiliary.db_interface import directories_to_db_values
 from gridpath.common_functions import create_results_df
@@ -55,7 +55,7 @@ def add_model_components(
 
     # Target specified as a scalar
     m.policy_requirement = Param(
-        m.POLICIES_ZONE_BLN_TYPE_HRZS_WITH_REQ, within=NonNegativeReals, default=0
+        m.POLICIES_ZONE_BLN_TYPE_HRZS_WITH_REQ, within=Reals, default=0
     )
 
     # Target specified as a function of load
